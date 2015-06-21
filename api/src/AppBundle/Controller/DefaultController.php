33502083623a46a840482c0bc7473712cd4f1b85
Samuel ROZE <samuel.roze@gmail.com>
2015-06-21 23:59:45 +0100

    Create the base authenticator application

---
+++ api/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        var_dump($this->getUser());
+        return new Response('OK');
+    }
+}
