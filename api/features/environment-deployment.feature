baabca3f265e571a409b510dfdbd055e122b57ec
Samuel ROZE <samuel.roze@gmail.com>
2015-07-11 14:53:59 +0100

    Create a DockerCompose package

---
+++ api/features/environment-deployment.feature
@@ -0,0 +1,7 @@
+Feature:
+  In order to have continuous delivery
+  As developer
+  I can create and update a given environment
+
+  Scenario:
+    Given I have no environment on my deployment target
--- api/src/ContinuousPipe/River/EnvironmentResolver.php
+++ api/src/ContinuousPipe/River/EnvironmentResolver.php
@@ -1,14 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sroze
- * Date: 08/07/15
- * Time: 13:45
- */
 
 namespace ContinuousPipe\River;
 
+use Builder\Image;
+use ContinuousPipe\Model\Environment;
 
-interface EnvironmentResolver {
-
+interface EnvironmentResolver
+{
+    /**
+     * Get the environment related to that given image.
+     *
+     * @param Image $image
+     *
+     * @return Environment
+     */
+    public function getEnvironmentForImage(Image $image);
 }
