a74d47fde2bc5e27d977539bb8438aa64a2a0da6
Samuel ROZE <samuel.roze@gmail.com>
2015-07-19 12:28:58 +0100

    Create an ApiBundle for authenticator API, and use FOS Rest

--- api/src/ApiBundle/Controller/UserController.php
+++ api/src/ApiBundle/Controller/UserController.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace ApiBundle\Controller;
 
 use AppBundle\Security\User\SecurityUserRepository;
 use JMS\Serializer\SerializerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations\View;
 
 /**
- * @Route(service="app.controller.user")
+ * @Route(service="api.controller.user")
  */
 class UserController
 {
@@ -16,27 +17,23 @@ class UserController
      * @var SecurityUserRepository
      */
     private $userRepository;
-    /**
-     * @var SerializerInterface
-     */
-    private $serializer;
 
-    public function __construct(SecurityUserRepository $userRepository, SerializerInterface $serializer)
+    /**
+     * @param SecurityUserRepository $userRepository
+     */
+    public function __construct(SecurityUserRepository $userRepository)
     {
         $this->userRepository = $userRepository;
-        $this->serializer = $serializer;
     }
 
     /**
      * @Route("/user/{email}", methods={"GET"})
+     * @View
      */
     public function getByEmailAction($email)
     {
         $securityUser = $this->userRepository->findOneByEmail($email);
-        $user = $securityUser->getUser();
 
-        return new Response($this->serializer->serialize($user, 'json'), 200, [
-            'Content-Type' => 'application/json',
-        ]);
+        return $securityUser->getUser();
     }
 }
---
+++ api/src/ApiBundle/DependencyInjection/ApiExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ApiBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+class ApiExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
