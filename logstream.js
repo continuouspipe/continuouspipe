c95d099e8bc595391176991ca5b64dcc46a62381
Samuel ROZE <samuel.roze@gmail.com>
2015-07-28 15:01:59 +0100

    Create the base of real-time log stream

--- logstream.js
+++ logstream.js
@@ -1,19 +1,17 @@
-if (Meteor.isClient) {
-    // This code only runs on the client
-    angular.module('logstream',['angular-meteor']);
+Logs = new Mongo.Collection('logs');
 
-    angular.module('logstream').controller('LogsCtrl', ['$scope',
-        function ($scope) {
-            $scope.logs = [
-                { text: 'This is task 1' },
-                { text: 'This is task 2' },
-                { text: 'This is task 3' }
-            ];
-        }]);
+if (Meteor.isClient) {
+    angular.module('logstream', [
+        'angular-meteor'
+    ]);
+
+    angular.module('logstream').controller('LogsCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+        $scope.logs = $meteor.collection(Logs);
+    }]);
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
