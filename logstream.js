a8b3841d4d517537cd38b95f2f71c858af42b634
Samuel ROZE <samuel.roze@gmail.com>
2015-07-28 16:26:44 +0100

    UI-Router integration

--- logstream.js
+++ logstream.js
@@ -2,11 +2,27 @@ Logs = new Mongo.Collection('logs');
 
 if (Meteor.isClient) {
     angular.module('logstream', [
-        'angular-meteor'
-    ]);
+        'angular-meteor',
+        'ui.router'
+    ]).config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
 
-    angular.module('logstream').controller('LogsCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
-        $scope.logs = $meteor.collection(Logs);
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'client/home.ng.html'
+            })
+            .state('logs', {
+                url: '/log/:logId',
+                templateUrl: 'client/log.ng.html',
+                controller: 'LogCtrl'
+            })
+        ;
+    }]);
+
+    angular.module('logstream').controller('LogCtrl', ['$scope', '$meteor', '$stateParams', function ($scope, $meteor, $stateParams) {
+        $scope.logId = $stateParams.logId;
+//        $scope.logs = $meteor.collection(Logs);
     }]);
 }
 
