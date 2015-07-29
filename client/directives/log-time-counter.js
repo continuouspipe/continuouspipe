c17647029140ea2d3d789b7690a35f55c8d39a7d
Samuel ROZE <samuel.roze@gmail.com>
2015-07-29 15:35:01 +0100

    Display the time counter on the tasks

---
+++ client/directives/log-time-counter.js
@@ -0,0 +1,41 @@
+angular.module('logstream')
+    .directive('logTimeCounter', function($interval) {
+        return {
+            scope: {
+                logTimeCounter: '='
+            },
+            restrict: 'A',
+            template: '{{ timeInDate | date:\'HH:mm:ss\'}}',
+            controller: function($scope) {
+                var timeInterval = null;
+
+                $scope.$watchGroup(['logTimeCounter.updatedAt', 'logTimeCounter.createdAt'], function() {
+                    var until = $scope.logTimeCounter.updatedAt,
+                        from = $scope.logTimeCounter.createdAt;
+
+                    if (!until) {
+                        timeInterval = $interval(function () {
+                            updateTimeCounter(until, from);
+                        });
+                    } else if (timeInterval !== null) {
+                        $interval.cancel(timeInterval);
+                    }
+                });
+
+                function updateTimeCounter(until, from) {
+                    if (!until) {
+                        until = new Date();
+                    } else if (!(until instanceof Date)) {
+                        until = new Date(until);
+                    }
+
+                    if (!(from instanceof Date)) {
+                        from = new Date(from);
+                    }
+
+                    $scope.timeInSeconds = Math.round((until - from) / 1000);
+                    $scope.timeInDate = new Date(0, 0, 0, 0, 0, 0, 0).setSeconds($scope.timeInSeconds);
+                };
+            }
+        };
+    });
