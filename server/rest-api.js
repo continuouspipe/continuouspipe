0410a58b9e73740474cfbe2faee22946ffa73ccd
Samuel ROZE <samuel.roze@gmail.com>
2015-07-30 10:06:44 +0100

    Allow partial PUT on the log objects

--- server/rest-api.js
+++ server/rest-api.js
@@ -5,7 +5,41 @@ var Api = new Restivus({
 
 // Generates: GET, POST on /api/logs and GET, PUT, DELETE on
 // /api/logs/:id for Items collection
-Api.addCollection(Logs);
+Api.addCollection(Logs, {
+    endpoints: {
+        put: {
+            action: function() {
+                var objectIdentifier = this.urlParams.id,
+                    existingObject = Logs.findOne({_id: objectIdentifier}),
+                    objectChanges = this.bodyParams;
+
+                if (!objectChanges._id) {
+                    objectChanges._id = objectIdentifier;
+                }
+
+                // Get the different between the 2 objects
+                var diff = jsDiff2Mongo(existingObject, this.bodyParams),
+                    patch = diff[1];
+
+                // We want to apply and differential PUT, so remove the fields missing on the body
+                delete patch.$unset;
+
+                var isUpdated = Logs.update(objectIdentifier, patch);
+                if (isUpdated) {
+                    return {
+                        status: 'success',
+                        data: Logs.findOne({_id: objectIdentifier})
+                    };
+                }
+
+                return {
+                    statusCode: 400,
+                    body: 'Unable to apply update'
+                };
+            }
+        }
+    }
+});
 
 Meteor.startup(function () {
 
