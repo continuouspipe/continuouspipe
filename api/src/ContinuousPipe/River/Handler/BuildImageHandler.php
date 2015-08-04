91a7376b6345b0e4b6bfc64d4f8834fd021f7125
Samuel ROZE <samuel.roze@gmail.com>
2015-08-04 15:27:48 +0100

    Use logstream instead of previous CP logger in builder

--- api/src/ContinuousPipe/River/Handler/BuildImageHandler.php
+++ api/src/ContinuousPipe/River/Handler/BuildImageHandler.php
@@ -4,12 +4,15 @@ namespace ContinuousPipe\River\Handler;
 
 use ContinuousPipe\Builder\Client\BuilderClient;
 use ContinuousPipe\Builder\HttpNotification;
+use ContinuousPipe\Builder\Logging;
+use ContinuousPipe\Builder\LogStreamLogging;
 use ContinuousPipe\Builder\Notification;
 use ContinuousPipe\Builder\Request\BuildRequest;
 use ContinuousPipe\River\Command\BuildImageCommand;
 use ContinuousPipe\River\Event\Build\ImageBuildStarted;
 use ContinuousPipe\River\Repository\TideRepository;
 use ContinuousPipe\River\Tide;
+use LogStream\Log;
 use LogStream\LoggerFactory;
 use SimpleBus\Message\Bus\MessageBus;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -65,7 +68,7 @@ class BuildImageHandler
         $tideUuid = $command->getTideUuid();
         $tide = $this->tideRepository->find($tideUuid);
 
-        $buildRequest = $this->getBuildRequestWithNotificationConfiguration($tide, $command->getBuildRequest());
+        $buildRequest = $this->getBuildRequestWithNotificationConfiguration($tide, $command->getBuildRequest(), $logger->getLog());
         $build = $this->builderClient->build($buildRequest, $tide->getUser());
 
         $this->eventBus->handle(new ImageBuildStarted($tideUuid, $build));
@@ -74,20 +77,24 @@ class BuildImageHandler
     /**
      * Add the notification configuration to the created build request.
      *
-     * @param Tide         $tide
+     * @param Tide $tide
      * @param BuildRequest $buildRequest
+     * @param Log $parentLog
      *
      * @return BuildRequest
      */
-    private function getBuildRequestWithNotificationConfiguration(Tide $tide, BuildRequest $buildRequest)
+    private function getBuildRequestWithNotificationConfiguration(Tide $tide, BuildRequest $buildRequest, Log $parentLog)
     {
         $address = $this->urlGenerator->generate('builder_notification_post', [
             'tideUuid' => (string) $tide->getUuid(),
         ], UrlGeneratorInterface::ABSOLUTE_URL);
 
-        $httpNotification = HttpNotification::fromAddress($address);
-
-        $buildRequest = new BuildRequest($buildRequest->getRepository(), $buildRequest->getImage(), Notification::withHttp($httpNotification));
+        $buildRequest = new BuildRequest(
+            $buildRequest->getRepository(),
+            $buildRequest->getImage(),
+            Notification::withHttp(HttpNotification::fromAddress($address)),
+            Logging::withLogStream(LogStreamLogging::fromParentLogIdentifier($parentLog->getId()))
+        );
 
         return $buildRequest;
     }
