93cce25f0d4f984121a4f514f4778f20498baf39
Samuel ROZE <samuel.roze@gmail.com>
2015-08-05 14:35:02 +0100

    Display no children by default Add chevrons to display expanded or collapsed logs

--- client/directives/logs.js
+++ client/directives/logs.js
@@ -3,30 +3,16 @@ angular.module('logstream')
         return {
             restrict: 'E',
             scope: {
-                logId: '=',
-                counter: '='
+                logId: '='
             },
             templateUrl: 'client/views/logs/logs.ng.html',
             controller: ['$scope', function ($scope) {
-                $scope.childrenCounter = {count: 0};
                 $scope.displayChildrenOf = [];
                 $scope.toggleChildrenDisplay = function(log) {
                     $scope.displayChildrenOf[log._id] = !$scope.displayChildrenOf[log._id];
                 };
 
                 $scope.logs = LogRepository.findByParentId($scope.logId);
-
-                $scope.$watch('logs', function(logs) {
-                    if ($scope.counter) {
-                        $scope.counter.count = logs.length;
-                    }
-
-                    logs.forEach(function(log) {
-                        if ($scope.displayChildrenOf[log._id] === undefined) {
-                            $scope.displayChildrenOf[log._id] = true;
-                        }
-                    });
-                });
             }],
             compile: function(element) {
                 return RecursionHelper.compile(element);
