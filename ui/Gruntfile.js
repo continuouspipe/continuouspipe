2827028ac2b0ee8492970c0251b90d1e6c0a394b
Samuel ROZE <samuel.roze@gmail.com>
2015-08-06 11:00:57 +0100

    Allow to delete flows

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
         tasks: ['wiredep']
       },
       js: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+        files: ['<%= yeoman.app %>/{**/}*.js'],
         tasks: ['newer:jshint:all'],
         options: {
           livereload: '<%= connect.options.livereload %>'
--- ui/app/flow/models/repository/flow-repository.js
+++ ui/app/flow/models/repository/flow-repository.js
@@ -2,10 +2,13 @@
 
 angular.module('continuousPipeRiver')
     .service('FlowRepository', function($resource, RIVER_API_URL) {
-        this.resource = $resource(RIVER_API_URL+'/flows/:identifier', {identifier: '@id'});
+        this.resource = $resource(RIVER_API_URL+'/flows/:uuid', {identifier: '@id'});
 
         this.findAll = function() {
             return this.resource.query().$promise;
         };
-    });
 
+        this.remove = function(flow) {
+            return this.resource.delete({uuid: flow.uuid}).$promise;
+        };
+    });
