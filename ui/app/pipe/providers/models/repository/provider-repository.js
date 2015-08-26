0d349810172489c09dbdefc2e7d9abdf91b9d1e8
Samuel ROZE <samuel.roze@gmail.com>
2015-08-26 11:41:36 +0100

    Delete a provider

--- ui/app/pipe/providers/models/repository/provider-repository.js
+++ ui/app/pipe/providers/models/repository/provider-repository.js
@@ -2,14 +2,14 @@
 
 angular.module('continuousPipeRiver')
     .service('ProviderRepository', function($resource, PIPE_API_URL) {
-        this.resource = $resource(PIPE_API_URL+'/providers/:uuid');
+        this.resource = $resource(PIPE_API_URL+'/providers/:type/:identifier');
 
         this.findAll = function() {
             return this.resource.query().$promise;
         };
 
         this.remove = function(provider) {
-            return this.resource.delete({uuid: provider.uuid}).$promise;
+            return this.resource.delete({type: provider.type, identifier: provider.identifier}).$promise;
         };
 
         this.create = function(type, provider) {
