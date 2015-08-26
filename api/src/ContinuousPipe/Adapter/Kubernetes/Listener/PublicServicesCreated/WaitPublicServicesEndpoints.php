ad5b1ef87f0768b9969e53292d93c6c9618abc23
Samuel ROZE <samuel.roze@gmail.com>
2015-08-26 23:31:05 +0100

    The deployment is failed when we failed to have the public endpoints

--- api/src/ContinuousPipe/Adapter/Kubernetes/Listener/PublicServicesCreated/WaitPublicServicesEndpoints.php
+++ api/src/ContinuousPipe/Adapter/Kubernetes/Listener/PublicServicesCreated/WaitPublicServicesEndpoints.php
@@ -3,8 +3,13 @@
 namespace ContinuousPipe\Adapter\Kubernetes\Listener\PublicServicesCreated;
 
 use ContinuousPipe\Adapter\Kubernetes\Event\PublicServicesCreated;
+use ContinuousPipe\Adapter\Kubernetes\PublicEndpoint\EndpointNotFound;
 use ContinuousPipe\Adapter\Kubernetes\PublicEndpoint\ServiceWaiter;
+use ContinuousPipe\Pipe\DeploymentContext;
+use ContinuousPipe\Pipe\Environment\PublicEndpoint;
+use ContinuousPipe\Pipe\Event\DeploymentFailed;
 use ContinuousPipe\Pipe\Event\PublicEndpointsCreated;
+use Kubernetes\Client\Model\Service;
 use SimpleBus\Message\Bus\MessageBus;
 
 class WaitPublicServicesEndpoints
@@ -31,18 +36,32 @@ class WaitPublicServicesEndpoints
 
     /**
      * @param PublicServicesCreated $event
-     *
-     * @throws \Exception
      */
     public function notify(PublicServicesCreated $event)
     {
         $context = $event->getContext();
 
+        try {
+            $endpoints = $this->waitEndpoints($context, $event->getServices());
+
+            $this->eventBus->handle(new PublicEndpointsCreated($context, $endpoints));
+        } catch (EndpointNotFound $e) {
+            $this->eventBus->handle(new DeploymentFailed($context->getDeployment()->getUuid()));
+        }
+    }
+
+    /**
+     * @param DeploymentContext $context
+     * @param Service[] $services
+     * @return PublicEndpoint[]
+     */
+    private function waitEndpoints(DeploymentContext $context, array $services)
+    {
         $endpoints = [];
-        foreach ($event->getServices() as $service) {
+        foreach ($services as $service) {
             $endpoints[] = $this->waiter->waitService($context, $service);
         }
 
-        $this->eventBus->handle(new PublicEndpointsCreated($context, $endpoints));
+        return $endpoints;
     }
 }
