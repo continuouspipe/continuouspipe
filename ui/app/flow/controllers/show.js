b6a467cee54d92a791a6ac6dcf556f5de33cc76b
Samuel ROZE <samuel.roze@gmail.com>
2015-09-04 13:35:02 +0100

    Display the environments on the river

--- ui/app/flow/controllers/show.js
+++ ui/app/flow/controllers/show.js
@@ -1,10 +1,18 @@
 'use strict';
 
 angular.module('continuousPipeRiver')
-    .controller('FlowController', function($scope, $remoteResource, TideRepository, flow) {
+    .controller('FlowController', function($scope, $remoteResource, TideRepository, EnvironmentRepository, flow) {
         $scope.flow = flow;
 
         $remoteResource.load('tides', TideRepository.findByFlow(flow)).then(function (tides) {
             $scope.tides = tides;
+        });
+
+        $remoteResource.load('environments', EnvironmentRepository.findByFlow(flow)).then(function (environments) {
+            $scope.environments = environments.map(function(environment) {
+                environment.name = environment.name.substr(flow.uuid.length + 1);
+
+                return environment;
+            });
         });
     });
