6e4287743529bb122a7737b421c3eaff0adfc665
Vit Brunner <vit.brunner@gmail.com>
2015-09-14 15:46:42 +0200

    Get run uuid from object, not array

--- api/src/ContinuousPipe/Runner/HttpClient.php
+++ api/src/ContinuousPipe/Runner/HttpClient.php
@@ -66,7 +66,7 @@ class HttpClient implements Client
 
         $contents = json_decode($this->getResponseContents($response));
 
-        return Uuid::fromString($contents['uuid']);
+        return Uuid::fromString($contents->uuid);
     }
 
     /**
