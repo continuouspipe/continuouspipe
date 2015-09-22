65e6eea82a800cf2ed1460a0f5f4428141c10ccc
Samuel ROZE <samuel.roze@gmail.com>
2015-09-22 17:01:08 +0100

    Add missing migration

---
+++ api/app/DoctrineMigrations/Version20150922170048.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150922170048 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE flow_dto DROP tasks');
+        $this->addSql('ALTER TABLE tide_dto ADD configuration TEXT DEFAULT NULL');
+        $this->addSql('COMMENT ON COLUMN tide_dto.configuration IS \'(DC2Type:b64Object)\'');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE flow_dto ADD tasks TEXT NOT NULL');
+        $this->addSql('COMMENT ON COLUMN flow_dto.tasks IS \'(DC2Type:b64Object)\'');
+        $this->addSql('ALTER TABLE tide_dto DROP configuration');
+    }
+}
