c59b82b20b1458c88c07ef64789bcb6ad0208ff1
Samuel ROZE <samuel.roze@gmail.com>
2015-10-04 14:10:36 +0100

    Fixes the Tide spec

--- api/spec/ContinuousPipe/River/TideSpec.php
+++ api/spec/ContinuousPipe/River/TideSpec.php
@@ -6,14 +6,15 @@ use ContinuousPipe\River\Event\TideFailed;
 use ContinuousPipe\River\Event\TideStarted;
 use ContinuousPipe\River\Event\TideSuccessful;
 use ContinuousPipe\River\Task\TaskList;
+use ContinuousPipe\River\Task\TaskRunner;
 use PhpSpec\ObjectBehavior;
 use Rhumsaa\Uuid\Uuid;
 
 class TideSpec extends ObjectBehavior
 {
-    public function let(TaskList $taskList)
+    public function let(TaskRunner $taskRunner, TaskList $taskList)
     {
-        $this->beConstructedWith($taskList);
+        $this->beConstructedWith($taskRunner, $taskList);
     }
 
     public function it_should_be_running_after_receiving_the_started_event()
