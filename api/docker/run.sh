ef8043eb92423fc21197c594b352b75017355500
Samuel ROZE <samuel.roze@gmail.com>
2015-10-07 11:20:57 +0100

    Start Apache with right permissions

--- api/docker/run.sh
+++ api/docker/run.sh
@@ -4,9 +4,6 @@ set -xe
 # Update parameters based on environment variables
 composer run-script update-parameters
 
-# Fixes permissions
-rm -rf app/cache/* && rm -rf app/logs/* && \
-	chown -R www-data:www-data app/cache && chown -R www-data:www-data app/logs
+# Start Apache with the right permissions
+/app/docker/start_safe_perms -DFOREGROUND
 
-# Start Apache
-/usr/local/bin/apache2-foreground
