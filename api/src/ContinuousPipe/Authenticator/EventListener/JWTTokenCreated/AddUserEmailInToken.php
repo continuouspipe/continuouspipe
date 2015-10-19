47303b8b1f4dbbf40286528f8237a62f11c2366f
Samuel ROZE <samuel.roze@gmail.com>
2015-10-19 12:36:25 +0100

    Join the user's email in the JWT token

---
+++ api/src/ContinuousPipe/Authenticator/EventListener/JWTTokenCreated/AddUserEmailInToken.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ContinuousPipe\Authenticator\EventListener\JWTTokenCreated;
+
+use ContinuousPipe\Security\User\SecurityUser;
+use ContinuousPipe\Security\User\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class AddUserEmailInToken
+{
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $payload = $event->getData();
+        $user = $event->getUser();
+
+        if ($user instanceof SecurityUser) {
+            $payload['email'] = $user->getUser()->getEmail();
+        }
+
+        $event->setData($payload);
+    }
+}
