02d764ef8fdcff051f004c6687acba5f12802225
Samuel ROZE <samuel.roze@gmail.com>
2015-10-21 10:49:50 +0100

    Add a loading flag when dealing with registry credentials

--- ui/app/account/registry-credentials/controllers/create.js
+++ ui/app/account/registry-credentials/controllers/create.js
@@ -3,11 +3,14 @@
 angular.module('continuousPipeRiver')
     .controller('RegistryCredentialsCreateController', function($scope, $state, RegistryCredentialsRepository) {
         $scope.create = function(credentials) {
+            $scope.isLoading = true;
             RegistryCredentialsRepository.create(credentials).then(function() {
                 $state.go('registry-credentials');
             }, function(error) {
                 var message = ((error || {}).data || {}).message || "An unknown error occured while saving the credentials";
                 swal("Error !", message, "error");
+            })['finally'](function() {
+                $scope.isLoading = false;
             });
         };
     });
