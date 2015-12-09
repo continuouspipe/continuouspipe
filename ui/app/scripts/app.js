601866288ce5e0950b803a05f9b07b89f79cf7c0
Samuel ROZE <samuel.roze@gmail.com>
2015-12-09 15:35:02 +0000

    Add GA analytics tracker

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -21,12 +21,21 @@ angular
         'angular-jwt',
         'dndLists',
         'ui.ace',
-        'angular-md5'
+        'angular-md5',
+        'angular-google-analytics'
     ])
-    .config(function($urlRouterProvider, $breadcrumbProvider, $locationProvider) {
+    .config(function($urlRouterProvider, $breadcrumbProvider, $locationProvider, AnalyticsProvider) {
         $urlRouterProvider.otherwise('/flows');
         $locationProvider.html5Mode(true);
         $breadcrumbProvider.setOptions({
             includeAbstract: true
         });
-    });
+
+        AnalyticsProvider
+            .setAccount('UA-71216332-2')
+            .setPageEvent('$stateChangeSuccess')
+        ;
+    })
+    // We need to inject it at least once to have automatic tracking
+    .run(function(Analytics) {})
+;
