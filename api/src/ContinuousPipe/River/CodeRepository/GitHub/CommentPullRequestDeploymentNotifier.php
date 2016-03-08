1fe26299063dbe254bfeb86b7fab634c94a712ce
Samuel ROZE <samuel.roze@gmail.com>
2016-03-08 11:39:58 +0000

    Uses team bucket to get GitHub credentials when commenting the pull-request

--- api/src/ContinuousPipe/River/CodeRepository/GitHub/CommentPullRequestDeploymentNotifier.php
+++ api/src/ContinuousPipe/River/CodeRepository/GitHub/CommentPullRequestDeploymentNotifier.php
@@ -3,11 +3,15 @@
 namespace ContinuousPipe\River\CodeRepository\GitHub;
 
 use ContinuousPipe\Pipe\Client\Deployment;
+use ContinuousPipe\River\CodeRepository\NotificationException;
 use ContinuousPipe\River\Event\GitHub\CommentedTideFeedback;
 use ContinuousPipe\River\EventBus\EventStore;
 use ContinuousPipe\River\GitHub\ClientFactory;
+use ContinuousPipe\River\GitHub\UserCredentialsNotFound;
 use ContinuousPipe\River\Task\Deploy\Event\DeploymentSuccessful;
 use ContinuousPipe\River\View\TideRepository;
+use ContinuousPipe\Security\Credentials\BucketNotFound;
+use ContinuousPipe\Security\Credentials\BucketRepository;
 use GitHub\WebHook\Model\PullRequest;
 use GitHub\WebHook\Model\Repository;
 
@@ -29,15 +33,22 @@ class CommentPullRequestDeploymentNotifier implements PullRequestDeploymentNotif
     private $tideRepository;
 
     /**
-     * @param ClientFactory  $gitHubClientFactory
-     * @param EventStore     $eventStore
-     * @param TideRepository $tideRepository
+     * @var BucketRepository
      */
-    public function __construct(ClientFactory $gitHubClientFactory, EventStore $eventStore, TideRepository $tideRepository)
+    private $bucketRepository;
+
+    /**
+     * @param ClientFactory    $gitHubClientFactory
+     * @param EventStore       $eventStore
+     * @param TideRepository   $tideRepository
+     * @param BucketRepository $bucketRepository
+     */
+    public function __construct(ClientFactory $gitHubClientFactory, EventStore $eventStore, TideRepository $tideRepository, BucketRepository $bucketRepository)
     {
         $this->gitHubClientFactory = $gitHubClientFactory;
         $this->eventStore = $eventStore;
         $this->tideRepository = $tideRepository;
+        $this->bucketRepository = $bucketRepository;
     }
 
     /**
@@ -49,7 +60,17 @@ class CommentPullRequestDeploymentNotifier implements PullRequestDeploymentNotif
         $contents = $this->getCommentContents($deployment);
         $tide = $this->tideRepository->find($deploymentSuccessful->getTideUuid());
 
-        $client = $this->gitHubClientFactory->createClientForUser($tide->getUser());
+        try {
+            $bucket = $this->bucketRepository->find($tide->getTeam()->getBucketUuid());
+        } catch (BucketNotFound $e) {
+            throw new NotificationException('The credentials bucket do not exists', $e->getCode(), $e);
+        }
+
+        try {
+            $client = $this->gitHubClientFactory->createClientFromBucket($bucket);
+        } catch (UserCredentialsNotFound $e) {
+            throw new NotificationException('No valid GitHub credentials in bucket', $e->getCode(), $e);
+        }
 
         // Remove previous comments
         $commentEvents = $this->eventStore->findByTideUuidAndType($tide->getUuid(), CommentedTideFeedback::class);
