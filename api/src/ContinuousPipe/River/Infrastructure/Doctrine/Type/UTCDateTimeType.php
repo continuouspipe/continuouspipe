62bc7c42d04b79717232e6f9ff75998df8bc55c3
Samuel ROZE <samuel.roze@gmail.com>
2016-05-03 23:10:54 +0100

    Work with UTC datetimes only

---
+++ api/src/ContinuousPipe/River/Infrastructure/Doctrine/Type/UTCDateTimeType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace ContinuousPipe\River\Infrastructure\Doctrine\Type;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeType;
+
+class UTCDateTimeType extends DateTimeType
+{
+    static private $utc;
+
+    /**
+     * @param \DateTime $value
+     * @param AbstractPlatform $platform
+     *
+     * @return string
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value instanceof \DateTime) {
+            $value->setTimezone(self::getUtc());
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+
+    /**
+     * @param string $value
+     * @param AbstractPlatform $platform
+     *
+     * @return \DateTime
+     *
+     * @throws ConversionException
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value || $value instanceof \DateTime) {
+            return $value;
+        }
+
+        $converted = \DateTime::createFromFormat(
+            $platform->getDateTimeFormatString(),
+            $value,
+            self::getUTC()
+        );
+
+        if (!$converted) {
+            throw ConversionException::conversionFailedFormat(
+                $value,
+                $this->getName(),
+                $platform->getDateTimeFormatString()
+            );
+        }
+
+        return $converted;
+    }
+
+    /**
+     * @return \DateTimeZone
+     */
+    static private function getUTC()
+    {
+        if (self::$utc === null) {
+            self::$utc = new \DateTimeZone('UTC');
+        }
+
+        return self::$utc;
+    }
+}
