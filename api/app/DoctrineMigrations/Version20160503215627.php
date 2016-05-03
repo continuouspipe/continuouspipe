a94b5bc3629d6743d2fc716cfcb97251353c5d62
Samuel ROZE <samuel.roze@gmail.com>
2016-05-03 21:57:44 +0100

    The creation date have to be nullable, else the default value is the current time and it's updated on update

--- api/app/DoctrineMigrations/Version20160503215627.php
+++ api/app/DoctrineMigrations/Version20160503215627.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20160503215258 extends AbstractMigration
+class Version20160503215627 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,8 +18,7 @@ class Version20160503215258 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE event_dto CHANGE event_datetime event_datetime TIMESTAMP(6) WITHOUT TIME ZONE NOT NULL');
-        $this->addSql('ALTER TABLE tide_dto CHANGE creation_date creation_date TIMESTAMP(6) WITHOUT TIME ZONE NOT NULL');
+        $this->addSql('ALTER TABLE tide_dto CHANGE creation_date creation_date TIMESTAMP(6) NULL');
     }
 
     /**
@@ -30,7 +29,6 @@ class Version20160503215258 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE event_dto CHANGE event_datetime event_datetime TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT \'CURRENT_TIMESTAMP(6)\' NOT NULL');
-        $this->addSql('ALTER TABLE tide_dto CHANGE creation_date creation_date TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT \'CURRENT_TIMESTAMP(6)\' NOT NULL');
+        $this->addSql('ALTER TABLE tide_dto CHANGE creation_date creation_date TIMESTAMP(6) DEFAULT \'CURRENT_TIMESTAMP(6)\' NOT NULL');
     }
 }
