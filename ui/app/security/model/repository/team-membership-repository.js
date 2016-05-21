ca1bbf413b368c171774a0dbff8489047d36d35b
Samuel ROZE <samuel.roze@gmail.com>
2016-05-21 14:07:14 +0100

    Fixes the user permissions body when adding a user

--- ui/app/security/model/repository/team-membership-repository.js
+++ ui/app/security/model/repository/team-membership-repository.js
@@ -12,7 +12,7 @@ angular.module('continuousPipeRiver')
         });
 
         this.add = function(team, user, permissions) {
-            return this.resource.add({team: team.slug, username: user.username}, permissions).$promise
+            return this.resource.add({team: team.slug, username: user.username}, {permissions: permissions}).$promise
         };
 
         this.remove = function(team, user) {
