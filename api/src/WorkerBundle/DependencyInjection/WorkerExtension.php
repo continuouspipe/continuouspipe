944fcca91d46f4bf1c1dbf9e1e6b919f48dd6a3d
Samuel ROZE <samuel.roze@gmail.com>
2016-05-23 15:43:56 +0200

    Allow to delete an environment through the API

--- api/src/WorkerBundle/DependencyInjection/WorkerExtension.php
+++ api/src/WorkerBundle/DependencyInjection/WorkerExtension.php
@@ -14,7 +14,7 @@ class WorkerExtension extends Extension
      */
     public function load(array $config, ContainerBuilder $container)
     {
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('logging.xml');
     }
 }
