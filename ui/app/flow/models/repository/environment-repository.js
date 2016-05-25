edaaf42c63576684d72426f584a36de460e59601
Samuel ROZE <samuel.roze@gmail.com>
2016-05-25 20:17:46 +0200

    Allow to delete an environment from the list

--- ui/app/flow/models/repository/environment-repository.js
+++ ui/app/flow/models/repository/environment-repository.js
@@ -2,9 +2,20 @@
 
 angular.module('continuousPipeRiver')
     .service('EnvironmentRepository', function($resource, RIVER_API_URL) {
-        this.resource = $resource(RIVER_API_URL+'/flows/:uuid/environments/:name');
+        this.resource = $resource(RIVER_API_URL+'/flows/:uuid/environments/:name', {}, {
+            remove: {
+                method: 'DELETE'
+            }
+        });
 
         this.findByFlow = function(flow) {
             return this.resource.query({uuid: flow.uuid}).$promise
+        };
+
+        this.delete = function(flow, environment) {
+            return this.resource.remove({uuid: flow.uuid}, {
+                identifier: environment.identifier,
+                cluster: environment.cluster
+            }).$promise;
         };
     });
