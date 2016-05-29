1a26bfaf52ad058796865d9f91016b256415f72e
Samuel ROZE <samuel.roze@gmail.com>
2016-05-29 16:56:24 +0100

    Updates the migration in order to loop over the proper number of laps

--- api/app/DoctrineMigrations/Version20160529133433.php
+++ api/app/DoctrineMigrations/Version20160529133433.php
@@ -16,25 +16,6 @@ class Version20160529133433 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        $numberOfEvents = $this->connection->fetchAssoc('SELECT COUNT(*) as number FROM event_dto')['number'];
-        $numberOfEventsPerLoop = 100;
-        $numberOfLoops = ceil($numberOfEvents / $numberOfEvents);
-
-        for ($i = 0; $i < $numberOfLoops; $i++) {
-            $offset = ($i * $numberOfEventsPerLoop);
-
-            $results = $this->connection->fetchAll('SELECT id, serialized_event, event_datetime FROM event_dto LIMIT '.$numberOfEventsPerLoop.' OFFSET '.$offset);
-            foreach ($results as $row) {
-                $event = unserialize(base64_decode($row['serialized_event']));
-                $event = UuidReplacer::replace($event);
-
-                $row['serialized_event'] = base64_encode(serialize($event));
-                $this->connection->update('event_dto', [
-                    'serialized_event' => $row['serialized_event'],
-                    'event_datetime' => $row['event_datetime'],
-                ], ['id' => $row['id']]);
-            }
-        }
     }
 
     /**
