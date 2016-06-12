af7a771c6ad0e3f980782e6e39ed27289044c099
Samuel ROZE <samuel.roze@gmail.com>
2016-06-12 21:27:01 +0100

    Track and report console exceptions

---
+++ api/src/WorkerBundle/EventListener/ErrorLoggerListener.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WorkerBundle\EventListener;
+
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
+use Psr\Log\LoggerInterface;
+
+class ErrorLoggerListener
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param ConsoleTerminateEvent $event
+     */
+    public function onConsoleTerminate(ConsoleTerminateEvent $event)
+    {
+        $statusCode = $event->getExitCode();
+        $command = $event->getCommand();
+
+        if ($statusCode === 0) {
+            return;
+        }
+
+        if ($statusCode > 255) {
+            $statusCode = 255;
+            $event->setExitCode($statusCode);
+        }
+
+        $this->logger->warning(sprintf(
+            'Command `%s` exited with status code %d',
+            $command->getName(),
+            $statusCode
+        ));
+    }
+}
