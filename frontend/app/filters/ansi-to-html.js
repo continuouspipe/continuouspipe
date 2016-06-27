a423ed4dfa438d4774636943ee5ce642e885a0e9
Samuel ROZE <samuel.roze@gmail.com>
2016-06-27 15:24:00 +0100

    Delete the unused `ansi-to-html` filter

--- frontend/app/filters/ansi-to-html.js
+++
@@ -1,6 +0,0 @@
-angular.module('logstream')
-    .filter('ansiToHtml', ['$sce', function($sce) {
-        return function (value) {
-            return $sce.trustAsHtml(ansi_up.ansi_to_html(value));
-        };
-    }]);
