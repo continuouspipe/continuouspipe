ac720974946a47b1dcca21cd42c3e096c985e402
Samuel ROZE <samuel.roze@gmail.com>
2016-08-30 16:17:27 +0100

    The `/` path returns a 200

--- api/features/bootstrap/ApiContext.php
+++ api/features/bootstrap/ApiContext.php
@@ -5,6 +5,9 @@ use ContinuousPipe\Security\Tests\Authenticator\InMemoryAuthenticatorClient;
 use ContinuousPipe\Security\User\SecurityUser;
 use ContinuousPipe\Security\User\User;
 use Rhumsaa\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Security\Authentication\Token\JWTUserToken;
 
@@ -19,15 +22,26 @@ class ApiContext implements Context
      * @var InMemoryAuthenticatorClient
      */
     private $authenticatorClient;
+    /**
+     * @var KernelInterface
+     */
+    private $kernel;
 
     /**
-     * @param TokenStorageInterface       $tokenStorage
-     * @param InMemoryAuthenticatorClient $authenticatorClient
+     * @var Response|null
      */
-    public function __construct(TokenStorageInterface $tokenStorage, InMemoryAuthenticatorClient $authenticatorClient)
+    private $response;
+
+    /**
+     * @param TokenStorageInterface $tokenStorage
+     * @param InMemoryAuthenticatorClient $authenticatorClient
+     * @param KernelInterface $kernel
+     */
+    public function __construct(TokenStorageInterface $tokenStorage, InMemoryAuthenticatorClient $authenticatorClient, KernelInterface $kernel)
     {
         $this->tokenStorage = $tokenStorage;
         $this->authenticatorClient = $authenticatorClient;
+        $this->kernel = $kernel;
     }
 
     /**
@@ -42,5 +56,27 @@ class ApiContext implements Context
         $token->setUser(new SecurityUser($user));
 
         $this->tokenStorage->setToken($token);
+    }
+
+    /**
+     * @When I request the page at :path
+     */
+    public function iRequestThePageAt($path)
+    {
+        $this->response = $this->kernel->handle(Request::create($path));
+    }
+
+    /**
+     * @Then the response status code should be :statusCode
+     */
+    public function theResponseStatusCodeShouldBe($statusCode)
+    {
+        if ($this->response->getStatusCode() != $statusCode) {
+            throw new \RuntimeException(sprintf(
+                'Expected %d but got %d',
+                $statusCode,
+                $this->response->getStatusCode()
+            ));
+        }
     }
 }
