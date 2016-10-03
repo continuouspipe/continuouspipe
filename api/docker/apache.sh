494b3a0a4a510ec17ca79df4651ed211e96c2820
Samuel ROZE <samuel.roze@gmail.com>
2016-10-04 00:55:46 +0100

    [hotfix] Happened the PHP configuration to the correct file

--- api/docker/apache.sh
+++ api/docker/apache.sh
@@ -6,8 +6,8 @@ composer run-script update-parameters
 
 # Configure Tideways' API
 if [ -n "$TIDEWAYS_API_KEY" ]; then
-    echo "tideways.api_key = $TIDEWAYS_API_KEY" >> /etc/php5/apache2/php.ini
-    echo "tideways.connection = tcp://tideways:9135" >> /etc/php5/apache2/php.ini
+    echo "tideways.api_key = $TIDEWAYS_API_KEY" >> /usr/local/etc/php/php.ini
+    echo "tideways.connection = tcp://tideways:9135" >> /usr/local/etc/php/php.ini
 fi
 
 # Start Apache with the right permissions
