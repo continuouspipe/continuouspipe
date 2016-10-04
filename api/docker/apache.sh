cdf1e2f0d2e778193676bccd4564e8d7800838a8
Samuel ROZE <samuel.roze@gmail.com>
2016-10-04 10:27:30 +0100

    Configure Tideways' API for apache & cli

--- api/docker/apache.sh
+++ api/docker/apache.sh
@@ -8,8 +8,6 @@ composer run-script update-parameters
 if [ -n "$TIDEWAYS_API_KEY" ]; then
     echo "tideways.api_key = $TIDEWAYS_API_KEY" >> /etc/php/7.0/apache2/conf.d/40-tideways.ini
     echo "tideways.connection = tcp://tideways:9135" >> /etc/php/7.0/apache2/conf.d/40-tideways.ini
-
-    cp /etc/php/7.0/apache2/conf.d/40-tideways.ini /etc/php/7.0/cli/conf.d/40-tideways.ini
 fi
 
 # Start Apache with the right permissions
