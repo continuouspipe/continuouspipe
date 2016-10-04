d80e87a32b22a3ac4f4f51b2d3a606954fe5017d
Samuel ROZE <samuel.roze@gmail.com>
2016-10-04 10:22:44 +0100

    Uses PHP 7 is better

--- api/docker/apache.sh
+++ api/docker/apache.sh
@@ -6,8 +6,10 @@ composer run-script update-parameters
 
 # Configure Tideways' API
 if [ -n "$TIDEWAYS_API_KEY" ]; then
-    echo "tideways.api_key = $TIDEWAYS_API_KEY" >> /usr/local/etc/php/php.ini
-    echo "tideways.connection = tcp://tideways:9135" >> /usr/local/etc/php/php.ini
+    echo "tideways.api_key = $TIDEWAYS_API_KEY" >> /etc/php/7.0/apache2/conf.d/40-tideways.ini
+    echo "tideways.connection = tcp://tideways:9135" >> /etc/php/7.0/apache2/conf.d/40-tideways.ini
+
+    cp /etc/php/7.0/apache2/conf.d/40-tideways.ini /etc/php/7.0/cli/conf.d/40-tideways.ini
 fi
 
 # Start Apache with the right permissions
