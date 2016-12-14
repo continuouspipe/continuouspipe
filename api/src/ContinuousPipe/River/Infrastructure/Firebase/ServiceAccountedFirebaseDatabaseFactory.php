8248544c02ae7c05674edfc2c4b07ae1b0c7b83e
Samuel ROZE <samuel.roze@gmail.com>
2016-12-14 11:21:04 +0000

    Add the history middleware when possible

--- api/src/ContinuousPipe/River/Infrastructure/Firebase/ServiceAccountedFirebaseDatabaseFactory.php
+++ api/src/ContinuousPipe/River/Infrastructure/Firebase/ServiceAccountedFirebaseDatabaseFactory.php
@@ -3,9 +3,20 @@
 namespace ContinuousPipe\River\Infrastructure\Firebase;
 
 use Firebase\Database;
+use Firebase\Http\Middleware as FirebaseMiddleware;
+use Firebase\ServiceAccount;
+use Google\Auth\Credentials\ServiceAccountCredentials;
+use Google\Auth\Middleware\AuthTokenMiddleware;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
 
 final class ServiceAccountedFirebaseDatabaseFactory implements DatabaseFactory
 {
+    /**
+     * @var callable
+     */
+    private $historyMiddleware;
+
     /**
      * @var string
      */
@@ -13,10 +24,12 @@ final class ServiceAccountedFirebaseDatabaseFactory implements DatabaseFactory
 
     /**
      * @param string $serviceAccountPath
+     * @param callable $historyMiddleware
      */
-    public function __construct(string $serviceAccountPath)
+    public function __construct(string $serviceAccountPath, callable $historyMiddleware = null)
     {
         $this->serviceAccountPath = $serviceAccountPath;
+        $this->historyMiddleware = $historyMiddleware;
     }
 
     /**
@@ -24,6 +37,43 @@ final class ServiceAccountedFirebaseDatabaseFactory implements DatabaseFactory
      */
     public function create(string $uri): Database
     {
-        return \Firebase::fromServiceAccount($this->serviceAccountPath)->withDatabaseUri($uri)->getDatabase();
+        $googleAuthTokenMiddleware = $this->createGoogleAuthTokenMiddleware(ServiceAccount::fromValue($this->serviceAccountPath));
+
+        $stack = HandlerStack::create();
+        $stack->push(FirebaseMiddleware::ensureJson(), 'ensure_json');
+        $stack->push($googleAuthTokenMiddleware, 'auth_service_account');
+
+        if (null !== $this->historyMiddleware) {
+            $stack->push($this->historyMiddleware);
+        }
+
+        $http = new Client([
+            'base_uri' => $uri,
+            'handler' => $stack,
+            'auth' => 'google_auth',
+        ]);
+
+        return new Database(\GuzzleHttp\Psr7\uri_for($uri), new Database\ApiClient($http));
+    }
+
+    /**
+     * @param ServiceAccount $serviceAccount
+     *
+     * @return AuthTokenMiddleware
+     */
+    private function createGoogleAuthTokenMiddleware(ServiceAccount $serviceAccount)
+    {
+        $scopes = [
+            'https://www.googleapis.com/auth/userinfo.email',
+            'https://www.googleapis.com/auth/firebase.database',
+        ];
+
+        $credentials = [
+            'client_email' => $serviceAccount->getClientEmail(),
+            'client_id' => $serviceAccount->getClientId(),
+            'private_key' => $serviceAccount->getPrivateKey(),
+        ];
+
+        return new AuthTokenMiddleware(new ServiceAccountCredentials($scopes, $credentials));
     }
 }
