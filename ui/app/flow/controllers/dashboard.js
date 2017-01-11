a80c976a26a814231a4b17fb37234edb7d4e5ad1
Samuel ROZE <samuel.roze@gmail.com>
2017-01-11 12:39:18 +0000

    Wrap the dashboard content into the remote resource

--- ui/app/flow/controllers/dashboard.js
+++ ui/app/flow/controllers/dashboard.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('continuousPipeRiver')
-    .controller('FlowDashboardController', function($scope, $remoteResource, flow, $firebaseArray, $authenticatedFirebaseDatabase) {
+    .controller('FlowDashboardController', function($scope, $remoteResource, $q, flow, $firebaseArray, $authenticatedFirebaseDatabase) {
         $scope.flow = flow;
         $scope.tidesPerPipeline = [];
+        $scope.isLoading = true;
 
         var mergeTidesIntoOneArray = function() {
             var tides = [];
@@ -17,33 +18,49 @@ angular.module('continuousPipeRiver')
             $scope.tides = tides;
         };
 
+        var watchPipelineTides = function(database) {
+            var promises = [];
+
+            $scope.pipelines.forEach(function(pipeline) {
+                if (pipeline.lastTides) {
+                    return;
+                }
+
+                pipeline.lastTides = $firebaseArray(
+                    database.ref()
+                    .child('flows/'+flow.uuid+'/tides/by-pipelines/'+pipeline.uuid)
+                    .orderByChild('creation_date')
+                    .limitToLast(1)
+                );
+
+                $scope.tidesPerPipeline[pipeline.uuid] = $firebaseArray(
+                    database.ref()
+                    .child('flows/'+flow.uuid+'/tides/by-pipelines/'+pipeline.uuid)
+                    .orderByChild('creation_date')
+                    .limitToLast(10)
+                );
+
+                $scope.tidesPerPipeline[pipeline.uuid].$watch(mergeTidesIntoOneArray);
+
+                promises.push($scope.tidesPerPipeline[pipeline.uuid].$loaded);
+            });
+
+            return $q.all(promises);
+        };
+
         $remoteResource.load('tides', $authenticatedFirebaseDatabase.get(flow).then(function(database) {
             $scope.pipelines = $firebaseArray(
                 database.ref().child('flows/'+flow.uuid+'/pipelines')
             );
 
-            $scope.pipelines.$watch(function() {
-                $scope.pipelines.forEach(function(pipeline) {
-                    if (pipeline.lastTides) {
-                        return;
-                    }
-
-                    pipeline.lastTides = $firebaseArray(
-                        database.ref()
-                        .child('flows/'+flow.uuid+'/tides/by-pipelines/'+pipeline.uuid)
-                        .orderByChild('creation_date')
-                        .limitToLast(1)
-                    );
-
-                    $scope.tidesPerPipeline[pipeline.uuid] = $firebaseArray(
-                        database.ref()
-                        .child('flows/'+flow.uuid+'/tides/by-pipelines/'+pipeline.uuid)
-                        .orderByChild('creation_date')
-                        .limitToLast(10)
-                    );
-
-                    $scope.tidesPerPipeline[pipeline.uuid].$watch(mergeTidesIntoOneArray);
+            return $scope.pipelines.$loaded(function() {
+                $scope.pipelines.$watch(function() {
+                    watchPipelineTides(database);
                 });
+
+                return watchPipelineTides(database);
             });
+        }).then(function() {
+            $scope.isLoading = false;
         }));
     });
