17566f5d870c75fee8abe7f376ca12750f3b4286
Samuel ROZE <samuel.roze@gmail.com>
2017-01-13 15:03:28 +0000

    Prevent deep locking, as it could happen

---
+++ api/src/ContinuousPipe/River/Tide/Concurrency/Lock/PreventDeepLocking.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ContinuousPipe\River\Tide\Concurrency\Lock;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+class PreventDeepLocking implements Locker
+{
+    /**
+     * @var Locker
+     */
+    private $decoratedLocker;
+
+    /**
+     * @var Collection
+     */
+    private $lockedCollection;
+
+    /**
+     * @param Locker $decoratedLocker
+     */
+    public function __construct(Locker $decoratedLocker)
+    {
+        $this->decoratedLocker = $decoratedLocker;
+        $this->lockedCollection = new ArrayCollection();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function lock($name, callable $callable)
+    {
+        if ($this->lockedCollection->contains($name)) {
+            return $callable();
+        }
+
+        $this->lockedCollection->add($name);
+        return $this->decoratedLocker->lock($name, function() use ($name, $callable) {
+            try {
+                return $callable();
+            } finally {
+                $this->lockedCollection->removeElement($name);
+            }
+        });
+    }
+}
