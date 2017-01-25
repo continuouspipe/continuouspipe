b14214c332fc2f1113a87917c0f8d9185fe1e570
Geza Buza <gbuza@sessiondigital.com>
2017-01-25 14:59:49 +0100

    CD-190: Remove unused queue setup command

--- api/bootstrap.sh
+++ api/bootstrap.sh
@@ -10,6 +10,3 @@ docker-compose run api composer install
 
 # Run migrations
 docker-compose run api app/console doctrine:migrations:migrate --no-interaction
-
-# Setup the queue
-docker-compose run worker app/console rabbitmq:setup-fabric
