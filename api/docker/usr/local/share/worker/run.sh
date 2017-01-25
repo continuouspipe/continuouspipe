65c62e3f74d1aa598164d2789c04ed488aae1185
Samuel ROZE <samuel.roze@gmail.com>
2017-01-25 11:34:08 +0000

    Activate the Tideways profiler for workers

--- api/docker/usr/local/share/worker/run.sh
+++ api/docker/usr/local/share/worker/run.sh
@@ -2,4 +2,4 @@
 
 set -xe
 
-exec rabbitmq-cli-consumer -e "/app/app/console -e=prod --with-headers worker:consume" -i -c /usr/local/share/worker/configuration.conf
+exec rabbitmq-cli-consumer -e "/app/app/console -e=prod --with-headers worker:consume" -i --strict-exit-code -c /usr/local/share/worker/configuration.conf
