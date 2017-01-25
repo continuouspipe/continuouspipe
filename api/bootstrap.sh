bb3416312db6ee02d99e06995d8df3a0ce64b592
Geza Buza <gbuza@sessiondigital.com>
2017-01-25 14:58:44 +0100

    CD-190: Run Composer install inside the container
    Host might have different PHP version compared to what is expected in composer.json, or even does not have PHP installed at all.
    As a solution composer install should be run inside the application container.

--- api/bootstrap.sh
+++ api/bootstrap.sh
@@ -2,11 +2,11 @@
 
 set -xe
 
-# Install dependencies
-composer install
-
 # Startup the application
 docker-compose up -d
+
+# Install dependencies
+docker-compose run api composer install
 
 # Run migrations
 docker-compose run api app/console doctrine:migrations:migrate --no-interaction
