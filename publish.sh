c3af7ebcd7f3e7de62f0480f59e10b7477433f43
Tony Piper <tony.piper@antiphonal.net>
2017-01-26 12:24:37 +0000

    remove themes

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ if [[ $(git status -s) ]]
 then
 	    echo "The working directory is dirty. Please commit any pending changes."
 	        exit 1;
-	fi
+fi
 
 echo "Deleting old publication"
 rm -rf public
@@ -12,7 +12,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
