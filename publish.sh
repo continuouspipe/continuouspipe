806e4fd8d3590ec7e56559324e5a84210e21c4ee
Tony Piper <tony.piper@antiphonal.net>
2017-01-26 17:09:22 +0000

    tweaking publish

--- publish.sh
+++ publish.sh
@@ -21,4 +21,4 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push origin
