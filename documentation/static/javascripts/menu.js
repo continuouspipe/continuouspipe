117dd56487edae62726f33fd676e003e0d014228
Phillip Swaine <pswaine@inviqa.com>
2017-01-31 12:20:58 +0000

    Menu collapse functionality.

---
+++ documentation/static/javascripts/menu.js
@@ -0,0 +1,17 @@
+(function ($) {
+    $(document).ready(function($) {
+
+        function init() {
+            $('div.toc ul li ul').not(':has(a.current)').hide();
+            $('div.toc ul li span.section')
+                .css('cursor', function(){
+                    $(this).css("cursor","pointer");
+                })
+                .click(function(){
+                    $(this).parent().find('ul').toggle();
+                });
+        }
+
+        init();
+    });
+})(jQuery);
