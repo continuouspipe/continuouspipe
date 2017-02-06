bf2ed94cd4834d413974cc09da2779d616c887a7
Samuel ROZE <samuel.roze@gmail.com>
2017-02-06 15:09:53 +0000

    Uses CP build steps

--- ui/docker/run.sh
+++ ui/docker/run.sh
@@ -2,10 +2,14 @@
 set -xe
 
 # Updates the configuration file
-cd /app && \
-   grunt ngconstant && \
-   cp .tmp/scripts/config.js dist/scripts/config.js
+echo "
+angular.module('config', [])
+.constant('RIVER_API_URL', '//"$RIVER_API_URL"')
+.constant('AUTHENTICATOR_API_URL', '//"$AUTHENTICATOR_API_URL"')
+.constant('LOG_STREAM_API_URL', 'https://"$LOG_STREAM_API_URL"')
+.constant('PIPE_API_URL', '//"$PIPE_API_URL"')
+.constant('SENTRY_DSN', '"$SENTRY_DSN"')
+;" > /app/dist/scripts/config.js
 
 # Start nginx
 nginx -g "daemon off;"
-
