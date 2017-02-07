ba19197e98a8cd8e8912e7b5c2a0c62345c0a1a9
Samuel ROZE <samuel.roze@gmail.com>
2017-02-07 09:49:45 +0000

    Return alerts when the billing profile do not have active subscriptions

--- api/features/team/alerts.feature
+++ api/features/team/alerts.feature
@@ -7,7 +7,31 @@ Feature:
     Given I am authenticated as user "samuel"
     And there is a team "foo"
     And the user "samuel" is in the team "foo"
+    And there is a billing profile "00000000-0000-0000-0000-000000000000" for the user "samuel"
 
   Scenario: It alerts when the team do not have any billing profile
     When I request the alerts of the team "foo"
     Then I should see the "billing-profile-not-found" alert
+
+  Scenario: The billing profile have no subscription
+    Given the team "foo" is linked to the billing profile "00000000-0000-0000-0000-000000000000"
+    When I request the alerts of the team "foo"
+    Then I should see the "billing-profile-has-no-subscription" alert
+
+  Scenario: The team have no active subscription
+    Given the billing account "00000000-0000-0000-0000-000000000000" have the following subscriptions:
+      | plan        | quantity | state     |
+      | single-user | 1        | cancelled |
+    And the team "foo" is linked to the billing profile "00000000-0000-0000-0000-000000000000"
+    When I request the alerts of the team "foo"
+    Then I should see the "billing-profile-has-no-active-subscription" alert
+
+  Scenario: A team with an active subscription do not have any of these previous alerts
+    Given the billing account "00000000-0000-0000-0000-000000000000" have the following subscriptions:
+      | plan        | quantity | state  |
+      | single-user | 1        | active |
+    And the team "foo" is linked to the billing profile "00000000-0000-0000-0000-000000000000"
+    When I request the alerts of the team "foo"
+    Then I should not see the "billing-profile-has-no-active-subscription" alert
+    And I should not see the "billing-profile-has-no-subscription" alert
+    And I should not see the "billing-profile-not-found" alert
--- api/src/ContinuousPipe/Alerts/Billing/TeamBillingProfileAlertFinder.php
+++ api/src/ContinuousPipe/Alerts/Billing/TeamBillingProfileAlertFinder.php
@@ -7,6 +7,8 @@ use ContinuousPipe\Alerts\AlertAction;
 use ContinuousPipe\Alerts\AlertFinder;
 use ContinuousPipe\Billing\BillingProfile\UserBillingProfileNotFound;
 use ContinuousPipe\Billing\BillingProfile\UserBillingProfileRepository;
+use ContinuousPipe\Billing\Subscription\Subscription;
+use ContinuousPipe\Billing\Subscription\SubscriptionClient;
 use ContinuousPipe\Security\Team\Team;
 
 class TeamBillingProfileAlertFinder implements AlertFinder
@@ -15,10 +17,17 @@ class TeamBillingProfileAlertFinder implements AlertFinder
      * @var UserBillingProfileRepository
      */
     private $userBillingProfileRepository;
+    /**
+     * @var SubscriptionClient
+     */
+    private $subscriptionClient;
 
-    public function __construct(UserBillingProfileRepository $userBillingProfileRepository)
-    {
+    public function __construct(
+        UserBillingProfileRepository $userBillingProfileRepository,
+        SubscriptionClient $subscriptionClient
+    ) {
         $this->userBillingProfileRepository = $userBillingProfileRepository;
+        $this->subscriptionClient = $subscriptionClient;
     }
 
     /**
@@ -26,23 +35,56 @@ class TeamBillingProfileAlertFinder implements AlertFinder
      */
     public function findByTeam(Team $team): array
     {
-        $alerts = [];
-
         try {
-            $this->userBillingProfileRepository->findByTeam($team);
+            $billingProfile = $this->userBillingProfileRepository->findByTeam($team);
         } catch (UserBillingProfileNotFound $e) {
+            return [
+                new Alert(
+                    'billing-profile-not-found',
+                    'The team does not have any billing profile. You\'ll have a very limited experience.',
+                    new \DateTime(),
+                    new AlertAction(
+                        'state',
+                        'Configure the team',
+                        'configuration'
+                    )
+                ),
+            ];
+        }
+
+        $alerts = [];
+        $subscriptions = $this->subscriptionClient->findSubscriptionsForBillingProfile($billingProfile);
+        if (0 === count($subscriptions)) {
             $alerts[] = new Alert(
-                'billing-profile-not-found',
-                'The team does not have any billing profile. You\'ll have a very limited experience.',
+                'billing-profile-has-no-subscription',
+                'The team billing profile do not have any subcription. You\'ll have a very limited experience.',
                 new \DateTime(),
                 new AlertAction(
                     'state',
-                    'Configure',
+                    'Configure the team',
+                    'configuration'
+                )
+            );
+        } elseif (!$this->hasActiveSubscription($subscriptions)) {
+            $alerts[] = new Alert(
+                'billing-profile-has-no-active-subscription',
+                'The team billing profile subscription is not active. You\'ll have a very limited experience.',
+                new \DateTime(),
+                new AlertAction(
+                    'state',
+                    'Configure the team',
                     'configuration'
                 )
             );
         }
 
         return $alerts;
+    }
+
+    private function hasActiveSubscription(array $subscriptions) : bool
+    {
+        return array_reduce($subscriptions, function (bool $carry, Subscription $subscription) {
+            return $carry || $subscription->getState() == Subscription::STATE_ACTIVE;
+        }, false);
     }
 }
