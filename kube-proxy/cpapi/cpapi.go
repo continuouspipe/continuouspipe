6736fd1243c08156f3fbb8c3386b25ed2dea7b32
alessandrozucca <alessandro.zucca@gmail.com>
2017-02-14 14:37:17 +0000

    adding struct to Unmarshal the json that comes back from the api calls and adding api calls for fetching cluster information

--- kube-proxy/cpapi/cpapi.go
+++ kube-proxy/cpapi/cpapi.go
@@ -13,55 +13,154 @@ type ClusterInfoProvider interface {
 	GetClusterBasicAuthInfo(cpUsername string, cpApiKey string, teamName string, clusterIdentifier string) (clusterUser string, clusterPassword string)
 }
 
-type ClusterInfo struct{}
-
-func NewClusterInfo() *ClusterInfo {
-	return &ClusterInfo{}
+type ClusterInfo struct {
+	client *http.Client
 }
 
-type ApiUserResponse struct {
+func NewClusterInfo() *ClusterInfo {
+	clusterInfo := &ClusterInfo{}
+	clusterInfo.client = &http.Client{}
+	return clusterInfo
+}
+
+type ApiTeam struct {
+	Slug        string `json:"slug"`
+	Name        string `json:"name"`
+	BucketUuid  string      `json:"bucket_uuid"`
+	Memberships []ApiMembership `json:"memberships"`
+}
+
+type ApiMembership struct {
+	User       ApiUser `json:"user"`
+	Permission ApiPermission `json:"permissions"`
+}
+
+type ApiUser struct {
 	Username   string      `json:"username"`
 	Email      string      `json:"email"`
 	BucketUuid string      `json:"bucket_uuid"`
 	Roles      []string      `json:"roles"`
 }
 
+type ApiPermission struct {
+	Permission string `json:"permission"`
+}
+
+type ApiBucketClusters struct {
+	Clusters []ApiCluster `json:"clusters"`
+}
+
+type ApiCluster struct {
+	Identifier string    `json:"identifier"`
+	Address    string    `json:"address"`
+	Version    string    `json:"version"`
+	Username   string    `json:"username"`
+	Password   string    `json:"password"`
+	Type       string    `json:"type"`
+}
+
 //return the url of the cluster e.g. https://100.200.300.400/
 func (c ClusterInfo) GetClusterUrl(user string, apiKey string, teamName string, clusterIdentifier string) (*url.URL, error) {
-	//get the user bucket using his apiKye
-	url := &url.URL{
-		Scheme: "https",
-		Host:   "authenticator-staging.continuouspipe.io",
-		Path:   "/api/user/" + user,
+	//bucketUuid, err := c.getBucketUuid(user, apiKey, teamName, clusterIdentifier)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//if len(bucketUuid) == 0 {
+	//	return nil, fmt.Errorf("user bucket id not found")
+	//}
+
+	//clusterAddress, err := c.getClusterAddress(bucketUuid, apiKey)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//url, err := url.Parse(clusterAddress)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//
+	//return url, nil
+}
+
+func (c ClusterInfo) GetClusterBasicAuthInfo(cpUsername string, cpApiKey string, teamName string, clusterIdentifier string) (clusterUser string, clusterPassword string) {
+	return "", ""
+}
+
+func (c ClusterInfo) GetApiTeams() {
+	url := c.getAuthenticatorUrl()
+	url.Path = "/api/teams/"
+}
+
+func (c ClusterInfo) GetApiBucketClusters(bucketUuid string, apiKey string) (*ApiBucketClusters, error) {
+	url := c.getAuthenticatorUrl()
+	url.Path = "/api/bucket/" + bucketUuid + "/clusters"
+
+	req, err := http.NewRequest("GET", url.String(), nil)
+
+	//TODO: Master key to be taken from config
+	req.Header.Add("X-Api-Key", "")
+	if err != nil {
+		return nil, err
 	}
 
-	client := &http.Client{}
+	respBody, err := c.getResponseBody(c.client, req)
+	if err != nil {
+		return nil, err
+	}
+
+	bucketInfoResponse := &ApiBucketClusters{}
+	err = json.Unmarshal(respBody, bucketInfoResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return bucketInfoResponse, nil
+}
+
+func (c ClusterInfo) GetApiUser(user string, apiKey string) (*ApiUser, error) {
+	url := c.getAuthenticatorUrl()
+	url.Path = "/api/user/" + user
+
 	req, err := http.NewRequest("GET", url.String(), nil)
 	req.Header.Add("X-Api-Key", apiKey)
 	if err != nil {
 		return nil, err
 	}
+
+	respBody, err := c.getResponseBody(c.client, req)
+	if err != nil {
+		return nil, err
+	}
+
+	apiUserResponse := &ApiUser{}
+	err = json.Unmarshal(respBody, apiUserResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return apiUserResponse, nil
+}
+
+func (c ClusterInfo) getResponseBody(client *http.Client, req *http.Request) ([]byte, error) {
 	res, err := client.Do(req)
 	defer res.Body.Close()
 	if err != nil {
 		return nil, err
 	}
-
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Error requesting user information, request status: %s", res.StatusCode)
+	}
 	resBody, err := ioutil.ReadAll(res.Body)
-
-	apiUserResponse := ApiUserResponse{}
-	json.Unmarshal(resBody, &apiUserResponse)
-
-	fmt.Println(apiUserResponse.BucketUuid)
-
-	panic("die")
-
-	//get the cluster info using the master key
-	//https://authenticator-staging.continuouspipe.io/api/bucket/a0b40c68-eedc-11e6-97e2-0a580a840065/clusters?username=alessandrozucca&email=alessandro.zucca%40gmail.com
-
-	return nil, nil
+	if err != nil {
+		return nil, err
+	}
+	return resBody, nil
 }
 
-func (c ClusterInfo) GetClusterBasicAuthInfo(cpUsername string, cpApiKey string, teamName string, clusterIdentifier string) (clusterUser string, clusterPassword string) {
-	return "", ""
+func (c ClusterInfo) getAuthenticatorUrl() *url.URL {
+	return &url.URL{
+		Scheme: "https",
+		Host:   "authenticator-staging.continuouspipe.io",
+	}
 }
