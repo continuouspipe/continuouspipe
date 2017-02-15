a1bc1eae8e561b1af19c7f5466024ee0682ce7f5
BrianDGLS <briandgls92@gmail.com>
2017-02-15 10:26:33 +0000

    CD-225: billing alerts working for projects

--- ui/app/project/models/repository/alerts-repository.js
+++ ui/app/project/models/repository/alerts-repository.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('continuousPipeRiver')
-    .service('ProjectAlertsRepository', function($resource, RIVER_API_URL) {
-        this.resource = $resource(RIVER_API_URL+'/teams/:uuid/alerts');
+    .service('ProjectAlertsRepository', function($resource, AUTHENTICATOR_API_URL) {
+        this.resource = $resource(AUTHENTICATOR_API_URL+'/api/teams/:slug/alerts');
 
         this.findByProject = function(project) {
             return this.resource.query({
-                uuid: project.uuid
+                slug: project.slug
             }).$promise;
         };
     });
