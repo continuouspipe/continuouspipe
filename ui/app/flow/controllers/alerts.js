ca77b68960adef03839c501248b7cdf9998c102d
BrianDGLS <briandgls92@gmail.com>
2017-02-20 13:38:05 +0000

    CD-225: show project alerts on flow pages

--- ui/app/flow/controllers/alerts.js
+++ ui/app/flow/controllers/alerts.js
@@ -1,12 +1,22 @@
 'use strict';
 
 angular.module('continuousPipeRiver')
-    .controller('FlowAlertsController', function($scope, $state, AlertsRepository, flow) {
-        AlertsRepository.findByFlow(flow).then(function(alerts) {
-            $scope.alerts = alerts;
+    .controller('FlowAlertsController', function ($scope, $state, ProjectAlertsRepository, AlertsRepository, flow, project) {
+        $scope.alerts = [];
+
+        ProjectAlertsRepository.findByProject(project).then(function (alerts) {
+            alerts.forEach(function (alert) {
+                $scope.alerts.push(alert);
+            });
         });
 
-        $scope.actionAlert = function(action) {
+        AlertsRepository.findByFlow(flow).then(function (alerts) {
+            alerts.forEach(function (alert) {
+                $scope.alerts.push(alert);
+            });
+        });
+
+        $scope.actionAlert = function (action) {
             if (action.type == 'link') {
                 window.open(action.href, '_blank');
             } else if (action.type == 'state') {
