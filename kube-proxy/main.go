e9d93d81187ab41bad3250fe42d2e68559a90b03
alessandrozucca <alessandro.zucca@gmail.com>
2017-02-20 17:52:37 +0000

    saving WILDCARD_SSL_CERT and WILDCARD_SSL_KEY, as files on startup so can be used in ListenAndServeTLS(). Updated continuous-pipe.yml to expose port 443

--- kube-proxy/main.go
+++ kube-proxy/main.go
@@ -5,8 +5,9 @@ package main
 //
 // Suggested settings
 //
-// KUBE_PROXY_LISTEN_HTTPS      		true for testing locally, false on cp
-// KUBE_PROXY_LISTEN_ADDRESS			https://localhost:80
+// WILDCARD_SSL_CERT
+// WILDCARD_SSL_KEY
+// KUBE_PROXY_LISTEN_ADDRESS			https://localhost:443
 // KUBE_PROXY_INSECURE_SKIP_VERIFY		true, unless we have valid certificates on the kubernetes side
 // KUBE_PROXY_AUTHENTICATOR_HOST		for testing authenticator-staging.continuouspipe.io, on live authenticator.continuouspipe.io
 // KUBE_PROXY_MASTER_API_KEY			cp master api key
@@ -18,12 +19,20 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"io/ioutil"
 )
 
+var envListenAddress, _ = os.LookupEnv("KUBE_PROXY_LISTEN_ADDRESS") //e.g.: https://localhost:80
+var envWildcardSSLCert, _ = os.LookupEnv("WILDCARD_SSL_CERT")
+var envWildcardSSLKey, _ = os.LookupEnv("WILDCARD_SSL_KEY")
+
+var sslCertFileName = "server.crt"
+var sslKeyFileName = "server.key"
+
 func main() {
-	//Set the option as environment variables
-	envListenHttps, _ := os.LookupEnv("KUBE_PROXY_LISTEN_HTTPS")
-	envListenAddress, _ := os.LookupEnv("KUBE_PROXY_LISTEN_ADDRESS") //e.g.: https://localhost:80
+	if envWildcardSSLCert != "" && envWildcardSSLKey != "" {
+		writeSSLCertAndKey()
+	}
 
 	listenURL, err := url.Parse(envListenAddress)
 	if err != nil {
@@ -31,14 +40,16 @@ func main() {
 	}
 	h := kproxy.NewHttpHandler()
 
-	if envListenHttps == "true" {
-		err = http.ListenAndServeTLS(listenURL.Host, "server.crt", "server.key", h)
-	} else {
-		err = http.ListenAndServe(listenURL.Host, h)
-	}
+	err = http.ListenAndServeTLS(listenURL.Host, sslCertFileName, sslKeyFileName, h)
 
 	if err != nil {
 		cplogs.V(5).Infof("Error when listening: %v", err.Error())
 	}
 	cplogs.Flush()
+}
+
+func writeSSLCertAndKey() {
+	cplogs.V(5).Infoln("Writing provided SSL Cert")
+	ioutil.WriteFile(sslCertFileName, []byte(envWildcardSSLCert), 0644)
+	ioutil.WriteFile(sslKeyFileName, []byte(envWildcardSSLKey), 0644)
 }
