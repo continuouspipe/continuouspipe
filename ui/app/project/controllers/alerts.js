73ea30ea98a7575eeef2794c6b907090d856a6cf
BrianDGLS <briandgls92@gmail.com>
2017-02-21 10:55:38 +0000

    CD-226: show alerts dialog

--- ui/app/project/controllers/alerts.js
+++ ui/app/project/controllers/alerts.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('continuousPipeRiver')
-    .controller('ProjectAlertsController', function ($scope, $state, ProjectAlertsRepository, project) {
+    .controller('ProjectAlertsController', function ($scope, $state, ProjectAlertsRepository, project, $mdDialog) {
         ProjectAlertsRepository.findByProject(project).then(function (alerts) {
             $scope.alerts = alerts;
+            console.log(alerts[0]);
+            for (var i = 0; i < 3; i++) {
+                $scope.alerts.push(alert);
+            }
         });
 
         $scope.actionAlert = function (action) {
@@ -12,5 +16,19 @@ angular.module('continuousPipeRiver')
             } else if (action.type == 'state') {
                 $state.go(action.href);
             }
+        };
+
+        $scope.showAlerts = function () {
+            $mdDialog.show({
+                controller: function ($scope, $mdDialog) {
+                    $scope.close = function () {
+                        $mdDialog.cancel();
+                    };
+                },
+                templateUrl: 'project/views/dialogs/alert-list.html',
+                parent: angular.element(document.body),
+                clickOutsideToClose: true,
+                scope: $scope
+            });
         };
     });
