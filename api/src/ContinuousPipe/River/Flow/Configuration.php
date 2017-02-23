c08c640a24be547dc4c1d55688c2dd5f64d854a0
Andy Thompson <athompson@inviqa.com>
2017-02-23 17:55:57 +0000

    Add pre-normalisation of environment_variables from hash to array
    This is so as to simplify environment_variable declaration and allow in hash form for yaml anchors to be used in a more obvious and overrideable way.

--- api/src/ContinuousPipe/River/Flow/Configuration.php
+++ api/src/ContinuousPipe/River/Flow/Configuration.php
@@ -106,6 +106,20 @@ class Configuration implements ConfigurationInterface
         $node = $builder->root($name, 'variables', $nodeBuilder);
 
         $node
+            ->beforeNormalization()
+                ->ifArray()
+                ->then(function ($array) {
+                    foreach ($array as $key => $value) {
+                        if (is_string($key) && !is_array($value)) {
+                            $array[$key] = [
+                                'name' => $key,
+                                'value' => $value,
+                            ];
+                        }
+                    }
+                    return $array;
+                })
+            ->end()
             ->prototype('array')
                 ->children()
                     ->scalarNode('name')->isRequired()->end()
