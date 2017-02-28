e60195b42a1d5f56a9a2cc7782590e02b9cc1295
BrianDGLS <briandgls92@gmail.com>
2017-02-28 11:53:04 +0000

    CD-236: Add design for dev env section

--- ui/app/flow/controllers/remote.js
+++ ui/app/flow/controllers/remote.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('continuousPipeRiver')
-    .controller('FlowRemoteController', function($scope, $remoteResource, RemoteRepository, flow) {
+    .controller('FlowRemoteController', function ($scope, $remoteResource, RemoteRepository, flow) {
+        $scope.environments = [];
+
+        RemoteRepository.getDevEnvironments(flow).then(function (environemnts) {
+            environemnts.forEach(function (env) {
+                console.log(env);
+                $scope.environments.push(env);
+            });
+        });
+
+        $scope.getToken = function() {
+
+        };
     })
 ;
--- ui/app/flow/models/repository/remote-repository.js
+++ ui/app/flow/models/repository/remote-repository.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('continuousPipeRiver')
-    .service('RemoteRepository', function ($http, RIVER_API_URL, $mdToast) {
+    .service('RemoteRepository', function (RIVER_API_URL, $resource) {
+        this.resource = $resource(RIVER_API_URL + '/flows/:uuid/development-environments');
+
+        this.getDevEnvironments = function (flow) {
+            return this.resource.query({uuid: flow.uuid}).$promise;
+        };
     })
 ;
