029e0587a5d0e805d8842df5944f3fc678c0d266
BrianDGLS <briandgls92@gmail.com>
2017-03-01 13:05:26 +0000

    CD-236: Start integrating api with dev env page

--- ui/app/flow/controllers/remote.js
+++ ui/app/flow/controllers/remote.js
@@ -3,16 +3,29 @@
 angular.module('continuousPipeRiver')
     .controller('FlowRemoteController', function ($scope, $remoteResource, RemoteRepository, flow) {
         $scope.environments = [];
+        $scope.branchName = 'test';
+        $scope.token = '';
 
-        RemoteRepository.getDevEnvironments(flow).then(function (environemnts) {
-            environemnts.forEach(function (env) {
-                console.log(env);
+        RemoteRepository.getDevEnvironments(flow).then(function (environments) {
+            environments.forEach(function (env) {
                 $scope.environments.push(env);
             });
         });
 
-        $scope.getToken = function() {
+        $scope.getToken = function (branchName, flow) {
+            RemoteRepository.issueToken(branchName, flow).then(function (token) {
+                $scope.token = token;
+                console.log($scope.token);
+            });
+        };
 
+        $scope.createEnvironment = function (name, flow) {
+            RemoteRepository.createDevEnvironment(name, flow).then(function (env) {
+                $scope.environments.push(env);
+            });
+        };
+
+        $scope.delete = function (environment) {
         };
     })
 ;
--- ui/app/flow/models/repository/remote-repository.js
+++ ui/app/flow/models/repository/remote-repository.js
@@ -2,10 +2,34 @@
 
 angular.module('continuousPipeRiver')
     .service('RemoteRepository', function (RIVER_API_URL, $resource) {
-        this.resource = $resource(RIVER_API_URL + '/flows/:uuid/development-environments');
+        var API = RIVER_API_URL + '/flows/:uuid/development-environments';
+        // https://github.com/continuouspipe/river/pull/331#issue-210540884
 
         this.getDevEnvironments = function (flow) {
-            return this.resource.query({uuid: flow.uuid}).$promise;
+            return $resource(API).query({uuid: flow.uuid}).$promise;
+        };
+
+        this.issueToken = function (branchName, env, flow) {
+            return $resource(RIVER_API_URL + '/flows/:uuid/development-environments/:envUuid/initialization-token', {}, {
+                create: {
+                    method: 'POST',
+                    isArray: true
+                }
+            }).create({
+                uuid: flow.uuid,
+                envUuid: env.uuid
+            }, {git_branch: branchName}).$promise;
+        };
+
+        this.createDevEnvironment = function (name, flow) {
+            return $resource(API, {}, {
+                create: {
+                    method: 'POST',
+                    isArray: true
+                }
+            }).create({
+                uuid: flow.uuid
+            }, {name: name}).$promise;
         };
     })
 ;
