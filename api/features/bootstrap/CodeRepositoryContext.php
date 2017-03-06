6d9faf56b78821f2e4f10dd6df08779a960e1ba8
Zsolt Gál <zsolt.gal@sessiondigital.com>
2017-03-06 16:15:02 +0100

    Remove unnecessary changes

--- api/features/bootstrap/CodeRepositoryContext.php
+++ api/features/bootstrap/CodeRepositoryContext.php
@@ -8,6 +8,4 @@ interface CodeRepositoryContext extends Context
     public function thereIsARepositoryIdentified($identifier = null): CodeRepository;
 
     public function thereIsAFileContaining(string $path, string $contents);
-
-    public function aFileDoesNotExists(string $path);
 }
