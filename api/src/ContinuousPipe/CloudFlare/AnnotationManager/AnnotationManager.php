d0f96a19f52c34b6bd4fe338f43f0948de70b0fd
Samuel ROZE <samuel.roze@gmail.com>
2017-03-14 12:13:21 +0000

    Add the interface's comments

--- api/src/ContinuousPipe/CloudFlare/AnnotationManager/AnnotationManager.php
+++ api/src/ContinuousPipe/CloudFlare/AnnotationManager/AnnotationManager.php
@@ -19,7 +19,20 @@ interface AnnotationManager
      */
     public function readAnnotation(DeploymentContext $context, KubernetesObject $object, string $annotationName);
 
+    /**
+     * @param DeploymentContext $context
+     * @param KubernetesObject $object
+     * @param string $annotationName
+     * @param string $annotationValue
+     *
+     * @throws Exception
+     */
     public function writeAnnotation(DeploymentContext $context, KubernetesObject $object, string $annotationName, string $annotationValue);
 
+    /**
+     * @param KubernetesObject $object
+     *
+     * @return bool
+     */
     public function supports(KubernetesObject $object);
 }
