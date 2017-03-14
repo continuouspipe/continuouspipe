3119ac9eb532f4b9dd7a316fa124b3600944c897
Samuel ROZE <samuel.roze@gmail.com>
2017-03-14 13:59:57 +0000

    Add the number of flows and last tide in the teams table

--- api/src/AdminBundle/Controller/TeamController.php
+++ api/src/AdminBundle/Controller/TeamController.php
@@ -2,6 +2,10 @@
 
 namespace AdminBundle\Controller;
 
+use ContinuousPipe\River\Flow\Projections\FlatFlowRepository;
+use ContinuousPipe\River\View\Tide;
+use ContinuousPipe\River\View\TideRepository;
+use ContinuousPipe\Security\Team\Team;
 use ContinuousPipe\Security\Team\TeamRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -16,23 +20,60 @@ class TeamController
      * @var TeamRepository
      */
     private $teamRepository;
+    /**
+     * @var FlatFlowRepository
+     */
+    private $flowRepository;
+    /**
+     * @var TideRepository
+     */
+    private $tideRepository;
 
     /**
      * @param TeamRepository $teamRepository
+     * @param FlatFlowRepository $flowRepository
+     * @param TideRepository $tideRepository
      */
-    public function __construct(TeamRepository $teamRepository)
+    public function __construct(TeamRepository $teamRepository, FlatFlowRepository $flowRepository, TideRepository $tideRepository)
     {
         $this->teamRepository = $teamRepository;
+        $this->flowRepository = $flowRepository;
+        $this->tideRepository = $tideRepository;
     }
 
     /**
-     * @Route("/teams", name="admin_teams")
+     * @Route("/", name="admin_teams")
      * @Template
      */
     public function listAction()
     {
+        $items = array_map(function(Team $team) {
+            $flows = $this->flowRepository->findByTeam($team);
+
+            /** @var Tide|null $lastTide */
+            $lastTide = null;
+
+            foreach ($flows as $flow) {
+                $lastTides = $this->tideRepository->findLastByFlowUuid($flow->getUuid(), 1);
+
+                foreach ($lastTides as $tide) {
+                    if ($lastTide === null) {
+                        $lastTide = $tide;
+                    } elseif ($lastTide->getCreationDate() < $tide->getCreationDate()) {
+                        $lastTide = $tide;
+                    }
+                }
+            }
+
+            return [
+                'team' => $team,
+                'flows' => $flows,
+                'last_tide' => $lastTide,
+            ];
+        }, $this->teamRepository->findAll());
+
         return [
-            'teams' => $this->teamRepository->findAll(),
+            'items' => $items,
         ];
     }
 }
