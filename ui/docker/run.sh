0712516014892f89d9fd6bfebb96959c60b7a1c8
Samuel ROZE <samuel.roze@gmail.com>
2017-03-15 11:45:40 +0000

    Uses full addresses in the variables

--- ui/docker/run.sh
+++ ui/docker/run.sh
@@ -4,10 +4,10 @@ set -xe
 # Updates the configuration file
 echo "
 angular.module('config', [])
-.constant('RIVER_API_URL', '//"$RIVER_API_URL"')
-.constant('AUTHENTICATOR_API_URL', '//"$AUTHENTICATOR_API_URL"')
-.constant('LOG_STREAM_API_URL', 'https://"$LOG_STREAM_API_URL"')
-.constant('PIPE_API_URL', '//"$PIPE_API_URL"')
+.constant('RIVER_API_URL', '"$RIVER_API_URL"')
+.constant('AUTHENTICATOR_API_URL', '"$AUTHENTICATOR_API_URL"')
+.constant('LOG_STREAM_API_URL', '"$LOG_STREAM_API_URL"')
+.constant('PIPE_API_URL', '"$PIPE_API_URL"')
 .constant('SENTRY_DSN', '"$SENTRY_DSN"')
 ;" > /app/dist/scripts/config.js
 
