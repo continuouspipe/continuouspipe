9752025a358551bdddc6da54c672d27a3e4ba768
Samuel ROZE <samuel.roze@gmail.com>
2017-05-05 10:34:45 +0100

    Ignore fallback events errors, to support to remove it

--- api/src/ContinuousPipe/River/EventStore/FallbackToNonEmptyEventStore.php
+++ api/src/ContinuousPipe/River/EventStore/FallbackToNonEmptyEventStore.php
@@ -2,6 +2,8 @@
 
 namespace ContinuousPipe\River\EventStore;
 
+use Psr\Log\LoggerInterface;
+
 class FallbackToNonEmptyEventStore implements EventStore
 {
     /**
@@ -15,25 +17,47 @@ class FallbackToNonEmptyEventStore implements EventStore
     private $fallback;
 
     /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
      * @param EventStore $eventStore
      * @param EventStore $fallback
+     * @param LoggerInterface $logger
      */
-    public function __construct(EventStore $eventStore, EventStore $fallback)
+    public function __construct(EventStore $eventStore, EventStore $fallback, LoggerInterface $logger)
     {
         $this->eventStore = $eventStore;
         $this->fallback = $fallback;
+        $this->logger = $logger;
     }
 
     public function store(string $stream, $event)
     {
         $this->eventStore->store($stream, $event);
-        $this->fallback->store($stream, $event);
+
+        try {
+            $this->fallback->store($stream, $event);
+        } catch (\Throwable $e) {
+            $this->logger->warning('Unable to store events in fallback', [
+                'stream' => $stream,
+            ]);
+        }
     }
 
     public function read(string $stream): array
     {
         if (empty($events = $this->eventStore->read($stream))) {
-            $events = $this->fallback->read($stream);
+            try {
+                $events = $this->fallback->read($stream);
+            } catch (\Throwable $e) {
+                $this->logger->warning('Unable to get events from fallback, return 0 events', [
+                    'stream' => $stream,
+                ]);
+
+                $events = [];
+            }
         }
 
         return $events;
