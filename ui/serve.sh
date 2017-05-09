e160becc4d973fb287ad907c15ad85980f123af7
Elliot Wright <wright.elliot@gmail.com>
2017-05-09 13:15:34 +0100

    Updated serve script to allow variables to be overrided without modifying the script.

--- ui/serve.sh
+++ ui/serve.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-AUTHENTICATOR_API_URL=https://authenticator.continuouspipe.io RIVER_API_URL=https://river.continuouspipe.io grunt serve --port=80
+# Allow the user to override this script from their environment.
+: ${AUTHENTICATOR_API_URL_OVERRIDE:="https://authenticator.continuouspipe.io"}
+: ${RIVER_API_URL_OVERRIDE:="https://river.continuouspipe.io"}
+
+AUTHENTICATOR_API_URL="$AUTHENTICATOR_API_URL_OVERRIDE" RIVER_API_URL="$RIVER_API_URL_OVERRIDE" \
+    grunt serve --port=80
