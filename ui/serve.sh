9cba28c3d5ee9e2f86ff18ef29975a573ef97314
Tony Piper <tpiper@inviqa.com>
2017-05-10 11:04:50 +0100

    fix serve.sh

--- ui/serve.sh
+++ ui/serve.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-AUTHENTICATOR_API_URL=https://authenticator.continuouspipe.io RIVER_API_URL=https://river.continuouspipe.io grunt serve
+AUTHENTICATOR_API_URL=https://authenticator.continuouspipe.io RIVER_API_URL=https://river.continuouspipe.io grunt serve --port=80
