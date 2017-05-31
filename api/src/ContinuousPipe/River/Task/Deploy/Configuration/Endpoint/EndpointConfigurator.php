01007dab1cdd1134a48e3a11e39635db6ec734fe
Richard Miller <richardmiller@users.noreply.github.com>
2017-05-31 17:22:24 +0100

    remove phpstorm generated comment

--- api/src/ContinuousPipe/River/Task/Deploy/Configuration/Endpoint/EndpointConfigurator.php
+++ api/src/ContinuousPipe/River/Task/Deploy/Configuration/Endpoint/EndpointConfigurator.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rmiller
- * Date: 30/05/2017
- * Time: 17:07
- */
+
 namespace ContinuousPipe\River\Task\Deploy\Configuration\Endpoint;
 
 use ContinuousPipe\River\Pipeline\TideGenerationException;
