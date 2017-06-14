6a64c0f5c0567c9f309f8b32b36d04db13c2cdc0
Samuel ROZE <samuel.roze@gmail.com>
2017-06-14 13:13:47 +0100

    Uses the new branch pinning endpoints

--- ui/app/flow/models/repository/pinned-branch-repository.js
+++ ui/app/flow/models/repository/pinned-branch-repository.js
@@ -4,15 +4,15 @@ angular.module('continuousPipeRiver')
     .service('PinnedBranchRepository', function ($http, RIVER_API_URL, $mdToast) {
         this.pin = function (flowUuid, branch) {
             return $http({
-                method: 'POST',
-                url: RIVER_API_URL + '/flows/' + flowUuid + '/branch/' + encodeURIComponent(branch) + '/pinned'
+                method: 'PUT',
+                url: RIVER_API_URL + '/flows/' + flowUuid + '/pinned-branch/' + encodeURIComponent(branch)
             });
         };
 
         this.unpin = function (flowUuid, branch) {
             return $http({
                 method: 'DELETE',
-                url: RIVER_API_URL + '/flows/' + flowUuid + '/branch/' + encodeURIComponent(branch) + '/pinned'
+                url: RIVER_API_URL + '/flows/' + flowUuid + '/pinned-branch/' + encodeURIComponent(branch)
             });
         };
     });
