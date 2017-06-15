bba2ccd9805ba2ef31bf38f1205322c4a1685ee2
richardmiller <rmiller@inviqa.com>
2017-06-15 15:10:35 +0100

    Use updated pin/unpin endpoints

--- ui/app/flow/models/repository/pinned-branch-repository.js
+++ ui/app/flow/models/repository/pinned-branch-repository.js
@@ -3,16 +3,19 @@
 angular.module('continuousPipeRiver')
     .service('PinnedBranchRepository', function ($http, RIVER_API_URL, $mdToast) {
         this.pin = function (flowUuid, branch) {
-            return $http({
-                method: 'PUT',
-                url: RIVER_API_URL + '/flows/' + flowUuid + '/pinned-branch/' + encodeURIComponent(branch)
-            });
+            return $http.post(RIVER_API_URL + '/flows/' + flowUuid + '/pinned-branch',
+                { name: branch }
+            );
         };
 
         this.unpin = function (flowUuid, branch) {
             return $http({
                 method: 'DELETE',
-                url: RIVER_API_URL + '/flows/' + flowUuid + '/pinned-branch/' + encodeURIComponent(branch)
+                url: RIVER_API_URL + '/flows/' + flowUuid + '/pinned-branch',
+                data: { name: branch },
+                headers: {
+                    "Content-Type": "application/json"
+                }
             });
         };
     });
