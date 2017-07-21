f9f238eafb6bea458145ea863107711a7d7fc472
Samuel ROZE <samuel.roze@gmail.com>
2017-07-21 11:47:14 +0200

    Fixes #129: Hide the alerts when they disappear

--- ui/app/flow/controllers/alerts.js
+++ ui/app/flow/controllers/alerts.js
@@ -1,28 +1,37 @@
 'use strict';
 
 angular.module('continuousPipeRiver')
-    .controller('FlowAlertsController', function ($rootScope, $scope, $state, ProjectAlertsRepository, AlertsRepository, AlertManager, flow, project) {
+    .controller('FlowAlertsController', function ($rootScope, $scope, $state, $q, ProjectAlertsRepository, AlertsRepository, AlertManager, flow, project) {
         $scope.alerts = [];
 
-        function filterUniqueAlerts(alerts) {
-            alerts
-                .filter(function (alert) {
-                    return !$scope.alerts.filter(function (inScope) {
-                        return inScope.message === alert.message;
-                    }).length;
-                })
-                .forEach(function (alert) {
-                    $scope.alerts.push(alert);
-                });
+        function alertAlreadyExists(collection, alert) {
+            return collection.filter(function(collectionAlert) {
+                return collectionAlert.message === alert.message;
+            }).length > 0;
+        }
+
+        function uniqueAlerts(alerts) {
+            var uAlerts = [];
+
+            alerts.forEach(function (alert) {
+                if (alertAlreadyExists(uAlerts, alert)) {
+                    return;
+                }
+
+                uAlerts.push(alert);
+            });
+
+            return uAlerts;
         }
 
         $scope.loadAlerts = function () {
-            ProjectAlertsRepository.findByProject(project).then(function (alerts) {
-                filterUniqueAlerts(alerts);
-            });
-
-            AlertsRepository.findByFlow(flow).then(function (alerts) {
-                filterUniqueAlerts(alerts);
+            $q.all([
+                AlertsRepository.findByFlow(flow),
+                ProjectAlertsRepository.findByProject(project)
+            ]).then(function(alertsCollection) {
+                $scope.alerts = uniqueAlerts(
+                    [].concat.apply([], alertsCollection)
+                );
             });
         };
 
