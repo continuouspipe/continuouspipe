9eb95d5a3389e811e5b020ff6f70ee2e5e5e6599
Samuel ROZE <samuel.roze@gmail.com>
2017-07-24 07:39:41 +0100

    Uses the shell from the UI

--- ui/app/logs/directives/pod-shell.js
+++ ui/app/logs/directives/pod-shell.js
@@ -11,7 +11,7 @@ angular.module('continuousPipeRiver')
             }
         };
     })
-    .directive('podShell', function() {
+    .directive('podShell', function($tokenStorage, $window, KUBE_PROXY_HOSTNAME) {
         return {
             templateUrl: 'logs/views/pod/shell.html',
             scope: {
@@ -19,35 +19,62 @@ angular.module('continuousPipeRiver')
                 pod: '='
             },
             controller: function($scope) {
+                var term = new Terminal();
+                term.open(document.getElementById('terminal-container'), true);
+
                 $scope.connect = function() {
                     $scope.hasStarted = true;
-                    $scope.isLoading = true;
+                    $scope.isConnected = true;
 
-                    term = new Terminal();
-                    term.on('data', function (data) {
-                        console.log('send', data);
-                        //socket.send(data);
-                    });
-                    term.open(document.getElementById('terminal-container'), true);
+                    var proxyWebSocketUri =
+                        'wss://'+KUBE_PROXY_HOSTNAME+
+                        '/'+$scope.environment.flow.uuid+'/'+$scope.environment.cluster+
+                        '/api/v1'+
+                        '/namespaces/'+$scope.environment.identifier+
+                        '/pods/'+$scope.pod.identifier+
+                        '/exec'+
+                        '?'+
+                        'stdout=1&stdin=1&stderr=1&tty=true&command='+encodeURIComponent('/bin/bash')+
+                        '&access_token='+$tokenStorage.get()
+                    ;
+
+                    var socket = new WebSocket(proxyWebSocketUri, 'base64.channel.k8s.io');
+
+                    term.clear();
                     term.setOption('cursorBlink', true);
-                    term.write("Connecting to container "+$scope.pod.identifier+"...");
 
                     setTimeout(function() {
                         term.fit();
                     }, 10);
-                    //term.resize(width, height);
 
-                    /**
-                    socket.onmessage = function (e) {
-                        term.write(e.data);
-                    };
-                    socket.onerror = function (error) {
-                        $scope.state.connected = false;
-                    };
-                    socket.onclose = function(evt) {
-                        $scope.state.connected = false;
-                    };
-                    **/
+                    angular.element($window).bind('resize', function(){
+                        term.fit();
+                    });
+
+                    socket.addEventListener('message', function(event) {
+                        var message = event.data,
+                            contents = atob(message.substr(1));
+
+                        term.write(contents);
+                    });
+
+                    term.on('data', function(data) {
+                        socket.send('0'+btoa(data));
+                    });
+
+                    socket.addEventListener('error', function(error) {
+                        console.log('error', error);
+                        $scope.isConnected = false;
+                    });
+
+                    socket.addEventListener('close', function() {
+                        console.log('Closed!');
+                        $scope.isConnected = false;
+                    });
+
+                    $scope.$on('$destroy', function() {
+                        socket.close();
+                    });
                 };
             }
         };
