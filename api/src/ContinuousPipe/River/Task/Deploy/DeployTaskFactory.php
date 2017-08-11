0b5f84494d853650db06d581098ca3a7115c873c
matt <mcurran@inviqa.com>
2017-08-11 10:52:54 +0100

    CD-492 use scalar node to fix issue with variable parsing

--- api/src/ContinuousPipe/River/Task/Deploy/DeployTaskFactory.php
+++ api/src/ContinuousPipe/River/Task/Deploy/DeployTaskFactory.php
@@ -148,7 +148,7 @@ class DeployTaskFactory implements TaskFactory
                                         ->addDefaultsIfNotSet()
                                         ->children()
                                             ->booleanNode('enabled')->defaultTrue()->end()
-                                            ->integerNode('number_of_replicas')->defaultNull()->end()
+                                            ->scalarNode('number_of_replicas')->defaultNull()->end()
                                         ->end()
                                     ->end()
                                     ->arrayNode('runtime_policy')
