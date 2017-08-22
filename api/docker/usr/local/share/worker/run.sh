c12b5180c36712cdca730acdae43d70f9fd8a4e4
Samuel ROZE <samuel.roze@gmail.com>
2017-08-22 18:27:20 +0100

    [hotfix] Change the console path to be the right one

--- api/docker/usr/local/share/worker/run.sh
+++ api/docker/usr/local/share/worker/run.sh
@@ -5,5 +5,5 @@ set -xe
 exec /usr/local/bin/puller \
     -google-project-id=continuous-pipe-1042 \
     -service-account-file-path=/app/app/var/google/river-pub-sub-service-account.json \
-    -script-path="/app/bin/console -e=prod continuouspipe:message:consume" \
+    -script-path="/app/app/console -e=prod continuouspipe:message:consume" \
     -subscription=$GOOGLE_PUB_SUB_SUBSCRIPTION_NAME
