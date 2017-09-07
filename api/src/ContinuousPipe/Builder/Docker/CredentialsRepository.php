994b64861b3f36767bb994a7ba3767251f497152
Samuel ROZE <samuel.roze@gmail.com>
2017-09-07 13:40:25 +0100

    Check existing image precense

--- api/src/ContinuousPipe/Builder/Docker/CredentialsRepository.php
+++ api/src/ContinuousPipe/Builder/Docker/CredentialsRepository.php
@@ -7,6 +7,7 @@ use ContinuousPipe\Builder\RegistryCredentials;
 use ContinuousPipe\Security\Authenticator\CredentialsNotFound;
 use ContinuousPipe\Security\Credentials\DockerRegistry;
 use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
 
 interface CredentialsRepository
 {
@@ -14,16 +15,16 @@ interface CredentialsRepository
      * Return registry credentials for the given image.
      *
      * @param Image $image
-     * @param Uuid  $credentialsBucketUuid
+     * @param UuidInterface $credentialsBucketUuid
      *
      * @throws CredentialsNotFound
      *
      * @return RegistryCredentials
      */
-    public function findByImage(Image $image, Uuid $credentialsBucketUuid);
+    public function findByImage(Image $image, UuidInterface $credentialsBucketUuid);
 
     /**
      * @throws CredentialsNotFound
      */
-    public function findRegistryByImage(Image $image, Uuid $credentialsBucketUuid): DockerRegistry;
+    public function findRegistryByImage(Image $image, UuidInterface $credentialsBucketUuid): DockerRegistry;
 }
--- api/src/ContinuousPipe/Builder/Image/InMemoryRegistry.php
+++ api/src/ContinuousPipe/Builder/Image/InMemoryRegistry.php
@@ -4,12 +4,13 @@ namespace ContinuousPipe\Builder\Image;
 
 use ContinuousPipe\Builder\Image;
 use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
 
 class InMemoryRegistry implements Registry
 {
     private $images = [];
 
-    public function containsImage(Uuid $credentialsBucket, Image $image): bool
+    public function containsImage(UuidInterface $credentialsBucket, Image $image): bool
     {
         return in_array(
             sprintf('%s:%s', $image->getName(), $image->getTag()),
