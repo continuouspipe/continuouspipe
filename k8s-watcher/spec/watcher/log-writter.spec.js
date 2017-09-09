2f3b98d00ed97519a79faafaca6c1e263bfe0b8d
Samuel ROZE <samuel.roze@gmail.com>
2017-09-09 15:06:56 +0100

    Throttle the log writter to prevent too high write hit rate

--- k8s-watcher/spec/watcher/log-writter.spec.js
+++ k8s-watcher/spec/watcher/log-writter.spec.js
@@ -1,26 +1,55 @@
 const sinon = require('sinon')
 const LogWritter = require('../../src/watcher/log-writter');
+const sleep = require('sleep');
 
 describe('LogWritter: write logs to Firebase', () => {
-    var firebaseEntry, firebaseEntryMock;
+    var firebasePushSpy;
     var writter;
 
     beforeEach(() => {
-        firebaseEntry = {
-            push: function(entry) {}
-        };
+        firebasePushSpy = sinon.spy();
 
-        firebaseEntryMock = sinon.mock(firebaseEntry)
-
-        writter = new LogWritter(firebaseEntry)
+        writter = new LogWritter({
+            push: firebasePushSpy
+        })
     })
 
     it('write text inside a `text` child', () => {
-        firebaseEntryMock.expects('push').once().withArgs({
+        writter.write('FOO');
+
+        sinon.assert.calledOnce(firebasePushSpy);
+        sinon.assert.calledWith(firebasePushSpy, {
             type: 'text',
             contents: 'FOO'
-        })
+        });
+    })
+
+    it('writes maximum every 100 ms', () => {
+        clock = sinon.useFakeTimers();
 
         writter.write('FOO');
+        writter.write('BAR');
+        clock.tick(50);
+        writter.write('BAZ');
+        clock.tick(50);
+        writter.write('AFTER');
+        clock.tick(100);
+
+        clock.restore();
+
+        sinon.assert.match(firebasePushSpy.args[0][0], {
+            type: 'text',
+            contents: 'FOO'
+        });
+
+        sinon.assert.match(firebasePushSpy.args[1][0], {
+            type: 'text',
+            contents: 'BARBAZ'
+        }); 
+
+        sinon.assert.match(firebasePushSpy.args[2][0], {
+            type: 'text',
+            contents: 'AFTER'
+        });        
     })
 })
