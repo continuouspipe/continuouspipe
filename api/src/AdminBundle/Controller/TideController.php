171f25b5e3bc97f260e5bff938395a6cba338fb2
Samuel ROZE <samuel.roze@gmail.com>
2017-09-12 13:50:03 +0100

    Squeeze in the "force start from admin" feature

--- api/src/AdminBundle/Controller/TideController.php
+++ api/src/AdminBundle/Controller/TideController.php
@@ -2,19 +2,21 @@
 
 namespace AdminBundle\Controller;
 
-use ContinuousPipe\River\Managed\Resources\Calculation\ResourceCalculator;
+use ContinuousPipe\River\Command\StartTideCommand;
 use ContinuousPipe\River\EventBus\EventStore;
 use ContinuousPipe\River\Flow;
-use ContinuousPipe\River\Managed\Resources\ResourceUsageResolver;
 use ContinuousPipe\River\View\TideRepository;
 use ContinuousPipe\Security\Team\Team;
 use Ramsey\Uuid\Uuid;
+use SimpleBus\Message\Bus\MessageBus;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * @Route(service="admin.controller.tide")
@@ -37,6 +39,16 @@ class TideController
     private $paginator;
 
     /**
+     * @var MessageBus
+     */
+    private $commandBus;
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
      * @var string
      */
     private $uiUrl;
@@ -51,12 +63,16 @@ class TideController
         TideRepository $tideRepository,
         EventStore $eventStore,
         PaginatorInterface $paginator,
+        MessageBus $commandBus,
+        UrlGeneratorInterface $urlGenerator,
         string $uiUrl
     ) {
         $this->tideRepository = $tideRepository;
         $this->eventStore = $eventStore;
         $this->paginator = $paginator;
         $this->uiUrl = $uiUrl;
+        $this->commandBus = $commandBus;
+        $this->urlGenerator = $urlGenerator;
     }
 
     /**
@@ -103,5 +119,25 @@ class TideController
             'tideLogsUrl' => $logsUrl,
             'events' => $this->eventStore->findByTideUuidWithMetadata($tideUuid),
         ];
+    }
+
+    /**
+     * @Route("/teams/{team}/flows/{flow}/tides/{uuid}/force-start", methods={"POST"}, name="admin_tide_force_start")
+     * @ParamConverter("team", converter="team", options={"slug"="team"})
+     * @ParamConverter("flow", converter="flow", options={"identifier"="flow", "flat"=true})
+     */
+    public function forceStartAction(Team $team, Flow\Projections\FlatFlow $flow, Request $request, $uuid)
+    {
+        $this->commandBus->handle(new StartTideCommand(Uuid::fromString($uuid)));
+
+        $request->getSession()->getFlashBag()->add('success', 'FlatFlow\'s tides\' logs successfully archived');
+
+        return new RedirectResponse(
+            $this->urlGenerator->generate('admin_tide', [
+                'team' => $team->getSlug(),
+                'flow' => (string) $flow->getUuid(),
+                'uuid' => $uuid,
+            ])
+        );
     }
 }
