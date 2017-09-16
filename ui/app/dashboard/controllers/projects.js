bbd1635b6950e099b0d13fd34965b33219e73a54
Samuel ROZE <samuel.roze@gmail.com>
2017-09-16 13:07:35 +0100

    Create the project without the slug

--- ui/app/dashboard/controllers/projects.js
+++ ui/app/dashboard/controllers/projects.js
@@ -1,12 +1,24 @@
 'use strict';
 
 angular.module('continuousPipeRiver')
-    .controller('ProjectsController', function ($scope, $remoteResource, ProjectRepository, $http) {
+    .controller('ProjectsController', function ($scope, $remoteResource, ProjectRepository, $http, $mdDialog) {
         $remoteResource.load('projects', ProjectRepository.findAll()).then(function (projects) {
             $scope.projects = projects;
         });
+
+        $scope.create = function(event) {
+            event.preventDefault();
+
+            $mdDialog.show({
+                controller: 'CreateProjectController',
+                templateUrl: 'dashboard/views/projects/create.modal.html',
+                parent: angular.element(document.body),
+                targetEvent: event,
+                clickOutsideToClose:true,
+            });
+        };
     })
-    .controller('CreateProjectController', function ($scope, $state, $http, Slug, ProjectRepository) {
+    .controller('CreateProjectController', function ($scope, $state, $http, $mdDialog, Slug, ProjectRepository) {
         $scope.$watch('project.name', function (name, previous) {
             if ($scope.project && (!$scope.project.slug || $scope.project.slug == Slug.slugify(previous))) {
                 $scope.project.slug = Slug.slugify(name);
@@ -21,10 +33,17 @@ angular.module('continuousPipeRiver')
                 Intercom('trackEvent', 'created-project', {
                     project: project
                 });
+
+                $mdDialog.hide();
             }, function (error) {
+                $scope.hasErrored = true;
                 swal("Error !", $http.getError(error) || "An unknown error occured while creating the project", "error");
             })['finally'](function () {
                 $scope.isLoading = false;
             });
+        };
+
+        $scope.cancel = function() {
+            $mdDialog.cancel();
         };
     });
