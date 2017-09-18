94c7ebab81ef61b0034903053a7d39ed7b8b1977
Samuel ROZE <samuel.roze@gmail.com>
2017-09-18 13:16:00 +0100

    Revert this horrible `app.php` change

--- api/web/app.php
+++ api/web/app.php
@@ -18,7 +18,7 @@ $apcLoader->register(true);
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
