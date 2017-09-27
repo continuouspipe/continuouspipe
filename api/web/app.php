b89f72517220b0ad46818dec54d2df1660189018
Samuel ROZE <samuel.roze@gmail.com>
2017-09-27 20:10:39 +0100

    [hotfix] `app.php` needs to create the `Request` earlier

--- api/web/app.php
+++ api/web/app.php
@@ -6,10 +6,9 @@ require __DIR__.'/../vendor/autoload.php';
 
 $kernel = new AppKernel('prod', false);
 
-// Trust the remote proxy
+$request = Request::createFromGlobals();
 Request::setTrustedProxies(array('127.0.0.1', $request->server->get('REMOTE_ADDR')));
 
-$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
