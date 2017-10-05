f3f7a2441f6d0e45958e928f9e7843d5607025fd
Samuel ROZE <samuel.roze@gmail.com>
2017-10-05 07:33:48 +0100

    Re-add the previous existing classes for `unserialize()` BC purposes

---
+++ api/src/ContinuousPipe/Builder/Request/Artifact.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ContinuousPipe\Builder\Request;
+
+/**
+ * @deprecated Keep this class for BC purposes with PHP-serialized models.
+ */
+class Artifact extends \ContinuousPipe\Builder\Artifact
+{
+}
