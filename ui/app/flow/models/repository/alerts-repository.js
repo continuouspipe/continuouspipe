d7ecfb7c335024747f8aba4c603ec07e5619b6bd
Samuel ROZE <samuel.roze@gmail.com>
2018-02-01 16:04:17 +0000

    Fixes #20: Only get alerts one request at a time

--- ui/app/flow/models/repository/alerts-repository.js
+++ ui/app/flow/models/repository/alerts-repository.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('continuousPipeRiver')
-    .service('AlertsRepository', function($resource, RIVER_API_URL) {
+    .service('AlertsRepository', function($resource, $httpUtils, RIVER_API_URL) {
         this.resource = $resource(RIVER_API_URL+'/flows/:uuid/alerts');
 
         this.findByFlow = function(flow) {
-            return this.resource.query({
-                uuid: flow.uuid
-            }).$promise;
+            return $httpUtils.oneAtATime('alerts-flow-'+flow.uuid, function() {
+                return this.resource.query({
+                    uuid: flow.uuid
+                }).$promise;
+            }.bind(this));
         };
     });
--- ui/app/project/models/repository/alerts-repository.js
+++ ui/app/project/models/repository/alerts-repository.js
@@ -1,12 +1,33 @@
 'use strict';
 
 angular.module('continuousPipeRiver')
-    .service('ProjectAlertsRepository', function($resource, RIVER_API_URL) {
+    .service('ProjectAlertsRepository', function($resource, $httpUtils, RIVER_API_URL) {
         this.resource = $resource(RIVER_API_URL+'/teams/:slug/alerts');
 
         this.findByProject = function(project) {
-            return this.resource.query({
-                slug: project.slug
-            }).$promise;
+            return $httpUtils.oneAtATime('alerts-project-'+project.slug, function() {
+                return this.resource.query({
+                    slug: project.slug
+                }).$promise;
+            }.bind(this));
+        };
+    })
+    .service('$httpUtils', function() {
+        var runningProcesses = {};
+
+        this.oneAtATime = function(identifier, callback) {
+            if (identifier in runningProcesses) {
+                return runningProcesses[identifier];
+            }
+
+            return runningProcesses[identifier] = callback().then(function(result) {
+                delete runningProcesses[identifier];
+
+                return result;
+            }, function(error) {
+                delete runningProcesses[identifier];
+
+                throw error;
+            });
         };
     });
