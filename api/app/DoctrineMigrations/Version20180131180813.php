ba39c8fe14e222107503c85522de516d02eaf19f
Samuel ROZE <samuel.roze@gmail.com>
2018-02-01 09:08:26 +0000

    Add the missing migrations for client certificate credentials

---
+++ api/app/DoctrineMigrations/Version20180131180813.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180131180813 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE cluster ADD credentials_username VARCHAR(255) DEFAULT NULL, ADD credentials_password VARCHAR(255) DEFAULT NULL, ADD credentials_client_certificate LONGTEXT DEFAULT NULL, ADD credentials_client_certificate_password LONGTEXT DEFAULT NULL, ADD credentials_google_cloud_service_account LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE cluster DROP credentials_username, DROP credentials_password, DROP credentials_client_certificate, DROP credentials_client_certificate_password, DROP credentials_google_cloud_service_account');
+    }
+}
